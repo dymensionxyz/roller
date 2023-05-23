6679a65898b70b62284f11cc0d113e6f3d4fb3bc
Itay Levy <itaylevy134@gmail.com>
2023-05-23 13:29:02 +0300

    cleaned the root file

--- cmd/root.go
+++ cmd/root.go
@@ -36,8 +36,6 @@ Use "roller [command] --help" for more information about a command.
 	`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -46,13 +44,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.roller.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
