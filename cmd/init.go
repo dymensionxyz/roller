5e065bd9dd8151cb4946b1891bb49c06ccd4da69
Itay Levy <itaylevy134@gmail.com>
2023-05-23 14:35:46 +0300

    Now the init has all the neccesary flags

--- cmd/init.go
+++ cmd/init.go
@@ -8,24 +8,22 @@ import (
 
 // define a string constant for for the hub rpc url
 const hubRPC = "https://rpc-hub-35c.dymension.xyz:443"
+
 var initCmd = &cobra.Command{
-	Use:   "init",
+	Use:   "init <chain-id>",
 	Short: "Initialize a rollapp configuration on your local machine",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
+	Args: cobra.ExactArgs(1),
 }
 
 func init() {
 	configCmd.AddCommand(initCmd)
-	/*
-	Add those flags github copilot:
-	- `**hub-rpc` (testnet hub)** - hub rpc endpoint.
-- **`light-node-endpoint`(generated, localhost:26659)** - The data availability light node endpoint. Runs an Arabica Celestia light node if not provided.
-- `**denom**` (**u + first three letters of the chain ID**) - The rollapp token denominator, for example `wei` in Ethereum.
-- `**key-prefix**` (**$denom**)- The `bech32` prefix of the rollapp keys.
-- `**rollapp-binary` (rollapp_evm)** - The rollapp binary.
-- `**decimals**` ($10^{18}$) - Will be used to calculate the default total supply and staking parameters.
-	 */
-	 initCmd.Flags().StringP("hub-rpc", "", hubRPC, "Dymension Hub rpc endpoint")
+	initCmd.Flags().StringP("hub-rpc", "", hubRPC, "Dymension Hub rpc endpoint")
+	initCmd.Flags().StringP("light-node-endpoint", "", "localhost:26659", "The data availability light node endpoint. Runs an Arabica Celestia light node if not provided.")
+	initCmd.Flags().StringP("denom", "", "", "The rollapp token smallest denominator, for example `wei` in Ethereum.")
+	initCmd.Flags().StringP("key-prefix", "", "", "The `bech32` prefix of the rollapp keys.")
+	initCmd.Flags().StringP("rollapp-binary", "", "", "The rollapp binary. Should be passed only if you built a custom rollapp.")
+	initCmd.Flags().Int64P("decimals", "", 18, "The number of decimal places a rollapp token supports.")
 }
