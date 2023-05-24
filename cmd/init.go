f296b84fbee14481133e54a1a513bf4f8eeb0505
Itay Levy <itaylevy134@gmail.com>
2023-05-24 10:07:35 +0300

    trying to configure the rpc url for the query

--- cmd/init.go
+++ cmd/init.go
@@ -1,19 +1,57 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 
+	// Import NewClientFromNode from client utils.go
+	cosmos_client "github.com/cosmos/cosmos-sdk/client"
+
+	"github.com/dymensionxyz/dymension/x/rollapp/types"
 	"github.com/spf13/cobra"
 )
 
-// define a string constant for for the hub rpc url
 const hubRPC = "https://rpc-hub-35c.dymension.xyz:443"
+
+// Create a function to validate a chain id string. it should return an error if the chain id is invalid.
+
+func validateChainID(chainID string, cmd *cobra.Command) error {
+	// create a new node flag already initiated with value http://localhost:26659
+	cmd.Flags().String("node", "https://rpc-hub-35c.dymension.xyz:443", "")
+	clientCtx := cosmos_client.GetClientContextFromCmd(cmd)
+	rpcURI := "https://rpc-hub-35c.dymension.xyz:443"
+	clientCtx = clientCtx.WithNodeURI(rpcURI)
+	client, err := cosmos_client.NewClientFromNode(rpcURI)
+	if err != nil {
+		return err
+	}
+
+	clientCtx = clientCtx.WithClient(client)
+	queryClient := types.NewQueryClient(clientCtx)
+	// create a list of cmd.Flags() plus a "node" string flag I want to set here
+
+	pageReq, _ := client.ReadPageRequest(cmd.Flags())
+	params := &types.QueryAllRollappRequest{
+		Pagination: pageReq,
+	}
+	res, err := queryClient.RollappAll(context.Background(), params)
+	if err != nil {
+		// print the error nicely
+		fmt.Println("Error while querying the server:")
+		fmt.Println(err)
+	}
+	// print the response nicely
+	fmt.Println("Response from the server:")
+	fmt.Println(res)
+	println(fmt.Sprintf("Initializing a rollapp configuration for chain %s", chainID))
+	return nil
+}
 
 var initCmd = &cobra.Command{
 	Use:   "init <chain-id>",
 	Short: "Initialize a rollapp configuration on your local machine",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		validateChainID(args[0], cmd)
 	},
 	Args: cobra.ExactArgs(1),
 }
--- cmd/root.go
+++ cmd/root.go
@@ -10,29 +10,7 @@ var rootCmd = &cobra.Command{
 	Use:   "roller",
 	Short: "A simple CLI tool to spin up a rollapp",
 	Long: `
-Roller CLI is a tool for registering and running autonomous rollapps built with Dymension RDK. Dymension CLI offers everything you need to scaffold, config, register, and run your rollapp.
-
-To get started, init a rollapp configuration on your local machine:
-
-roller init mars
-
-Usage:
-	roller [command]
-
-Available Commands:
-	scaffold    Scaffold a new boilerplate dymension-rdk rollapp
-	config      Commands to initialize and manage the rollapp config on your local machine
-	register    Register a rollapp with the Dymension RDK
-	run         Run a rollapp locally
-	connect     Connect a runnning rollapp to the Dymension hub via IBC
-	version     Print the current build information
-	help        Help about any command
-	completion  Generate the autocompletion script for the specified shell
-
-Flags:
-	-h, --help   help for roller
-
-Use "roller [command] --help" for more information about a command.
+Roller CLI is a tool for registering and running autonomous rollapps built with Dymension RDK. The Roller offers everything you need to scaffold, config, register, and run your rollapp.
 	`,
 }
 
