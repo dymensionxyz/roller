d625a8fb1b52e6a8c30cc8d00d3edeb1e7ed44d7
Itay Levy <itaylevy134@gmail.com>
2023-05-30 15:03:55 +0200

    removed unnecessary config

--- cmd/config/init/initializeRollappConfig.go
+++ cmd/config/init/initializeRollappConfig.go
@@ -21,8 +21,6 @@ func setRollappAppConfig(appConfigFilePath string, denom string) {
 	config, _ := toml.LoadFile(appConfigFilePath)
 	config.Set("minimum-gas-prices", "0"+denom)
 	config.Set("api.enable", "true")
-	config.Set("grpc.address", "0.0.0.0:8080")
-	config.Set("grpc-web.address", "0.0.0.0:8081")
 	file, _ := os.Create(appConfigFilePath)
 	file.WriteString(config.String())
 	file.Close()
