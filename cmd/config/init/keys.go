9b638a6f24cb9c118f1582a344b89197932a0749
Itay <itaylevy134@gmail.com>
2023-06-04 15:38:57 +0200

    feat: `roller config init` command output formatting (#23)

--- cmd/config/init/keys.go
+++ cmd/config/init/keys.go
@@ -6,30 +6,46 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/types/bech32"
 )
 
-func generateKeys(rollappId string, hubId string, excludeKeys ...string) error {
-	keys := getDefaultKeys(rollappId, hubId)
+func keyInfoToBech32Address(info keyring.Info, prefix string) (string, error) {
+	pk := info.GetPubKey()
+	bech32Address, err := bech32.ConvertAndEncode(prefix, pk.Bytes())
+	if err != nil {
+		return "", err
+	}
+	return bech32Address, nil
+}
+
+func generateKeys(rollappId string, hubId string, rollappKeyPrefix string, excludeKeys ...string) (map[string]string, error) {
+	keys := getDefaultKeysConfig(rollappId, hubId, rollappKeyPrefix)
 	excludeKeysMap := make(map[string]struct{})
 	for _, key := range excludeKeys {
 		excludeKeysMap[key] = struct{}{}
 	}
-
+	addresses := make(map[string]string)
 	for _, key := range keys {
 		if _, exists := excludeKeysMap[key.keyId]; !exists {
-			_, err := createKey(rollappId, key.dir, key.keyId, key.coinType)
+			keyInfo, err := createKey(rollappId, key.dir, key.keyId, key.coinType)
 			if err != nil {
-				return err
+				return nil, err
 			}
+			formattedAddress, err := keyInfoToBech32Address(keyInfo, key.prefix)
+			if err != nil {
+				return nil, err
+			}
+			addresses[key.keyId] = formattedAddress
 		}
 	}
-	return nil
+	return addresses, nil
 }
 
 type keyConfig struct {
 	dir      string
 	keyId    string
 	coinType uint32
+	prefix   string
 }
 
 func createKey(rollappId string, relativePath string, keyId string, coinType ...uint32) (keyring.Info, error) {
@@ -54,32 +70,37 @@ func createKey(rollappId string, relativePath string, keyId string, coinType ...
 	return info, nil
 }
 
-func getDefaultKeys(rollappId string, hubId string) []keyConfig {
+func getDefaultKeysConfig(rollappId string, hubId string, rollappKeyPrefix string) []keyConfig {
 	return []keyConfig{
 		{
 			dir:      configDirName.Rollapp,
 			keyId:    keyNames.HubSequencer,
 			coinType: cosmosDefaultCointype,
+			prefix:   keyPrefixes.Hub,
 		},
 		{
 			dir:      configDirName.Rollapp,
 			keyId:    keyNames.RollappSequencer,
 			coinType: evmCoinType,
+			prefix:   rollappKeyPrefix,
 		},
 		{
 			dir:      path.Join(configDirName.Relayer, relayerKeysDirName, rollappId),
 			keyId:    keyNames.HubRelayer,
 			coinType: cosmosDefaultCointype,
+			prefix:   keyPrefixes.Hub,
 		},
 		{
 			dir:      path.Join(configDirName.Relayer, relayerKeysDirName, hubId),
 			keyId:    keyNames.RollappRelayer,
 			coinType: evmCoinType,
+			prefix:   rollappKeyPrefix,
 		}, {
 
 			dir:      path.Join(configDirName.DALightNode, relayerKeysDirName),
 			keyId:    keyNames.DALightNode,
 			coinType: cosmosDefaultCointype,
+			prefix:   keyPrefixes.DA,
 		},
 	}
 }
