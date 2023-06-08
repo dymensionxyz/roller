81c8881f06922d654111c1ec52c44f7013a065c6
Itay <itaylevy134@gmail.com>
2023-06-08 10:22:08 +0200

    fix: Fixed running `roller config init` when root dir does not exist (#45)

--- cmd/config/init/prompt_existing_root.go
+++ cmd/config/init/prompt_existing_root.go
@@ -10,13 +10,20 @@ import (
 
 func dirNotEmpty(path string) (bool, error) {
 	info, err := os.Stat(path)
-	if err != nil || !info.IsDir() {
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
 		return false, err
 	}
+
+	if !info.IsDir() {
+		return false, fmt.Errorf("%s is not a directory", path)
+	}
+
 	files, err := ioutil.ReadDir(path)
 	return len(files) > 0, err
 }
-
 func promptOverwriteConfig(home string) (bool, error) {
 	prompt := promptui.Prompt{
 		Label:     fmt.Sprintf("Directory %s is not empty. Do you want to overwrite", home),
