a0d003b898d603ea5f540e055131e5af0d409ec6
Itay Levy <itaylevy134@gmail.com>
2023-06-25 11:28:33 +0200

    moved bash commands from err handling to bash commands

--- cmd/utils/bash_commands.go
+++ cmd/utils/bash_commands.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -86,4 +87,32 @@ func getLogger(filepath string) *log.Logger {
 	multiWriter := io.MultiWriter(lumberjackLogger)
 	logger := log.New(multiWriter, "", log.LstdFlags)
 	return logger
+}
+
+func ExecBashCommand(cmd *exec.Cmd) (bytes.Buffer, error) {
+	var stderr bytes.Buffer
+	var stdout bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	if err != nil {
+		return stdout, fmt.Errorf("command execution failed: %w, stderr: %s", err, stderr.String())
+	}
+	return stdout, nil
+}
+
+func ExecBashCmdWithOSOutput(cmd *exec.Cmd, options ...CommandOption) error {
+	for _, option := range options {
+		option(cmd)
+	}
+	var stderr bytes.Buffer
+	outmw := io.MultiWriter(cmd.Stdout, os.Stdout)
+	cmd.Stdout = outmw
+	errmw := io.MultiWriter(os.Stderr, &stderr, cmd.Stderr)
+	cmd.Stderr = errmw
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("command execution failed: %w, stderr: %s", err, stderr.String())
+	}
+	return nil
 }
--- cmd/utils/error_handling.go
+++ cmd/utils/error_handling.go
@@ -1,13 +1,8 @@
 package utils
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-
-	"bytes"
 	"github.com/fatih/color"
+	"os"
 )
 
 func PrettifyErrorIfExists(err error) {
@@ -20,32 +15,4 @@ func PrettifyErrorIfExists(err error) {
 		color.New(color.FgRed, color.Bold).Printf("💈 %s\n", err.Error())
 		panic(err)
 	}
-}
-
-func ExecBashCommand(cmd *exec.Cmd) (bytes.Buffer, error) {
-	var stderr bytes.Buffer
-	var stdout bytes.Buffer
-	cmd.Stderr = &stderr
-	cmd.Stdout = &stdout
-	err := cmd.Run()
-	if err != nil {
-		return stdout, fmt.Errorf("command execution failed: %w, stderr: %s", err, stderr.String())
-	}
-	return stdout, nil
-}
-
-func ExecBashCmdWithOSOutput(cmd *exec.Cmd, options ...CommandOption) error {
-	for _, option := range options {
-		option(cmd)
-	}
-	var stderr bytes.Buffer
-	outmw := io.MultiWriter(cmd.Stdout, os.Stdout)
-	cmd.Stdout = outmw
-	errmw := io.MultiWriter(os.Stderr, &stderr, cmd.Stderr)
-	cmd.Stderr = errmw
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("command execution failed: %w, stderr: %s", err, stderr.String())
-	}
-	return nil
 }
