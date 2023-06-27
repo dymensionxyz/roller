997af6156c606064caa35e9126704a3cfdd74f36
Itay <itaylevy134@gmail.com>
2023-06-27 10:20:32 +0200

    feat: Add automatic rollapp relayer funding in the genesis file (#95)

--- cmd/config/init/flags.go
+++ cmd/config/init/flags.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dymensionxyz/roller/cmd/consts"
 	"github.com/dymensionxyz/roller/cmd/utils"
 	"github.com/spf13/cobra"
+	"math/big"
 )
 
 func addFlags(cmd *cobra.Command) {
@@ -15,12 +16,13 @@ func addFlags(cmd *cobra.Command) {
 	cmd.Flags().StringP(FlagNames.TokenSupply, "", "1000000000", "The total token supply of the RollApp")
 
 	cmd.PreRunE = func(cmd *cobra.Command, args []string) error {
-		hubID, err := cmd.Flags().GetString(FlagNames.HubID)
+		err := verifyHubID(cmd)
 		if err != nil {
 			return err
 		}
-		if _, ok := Hubs[hubID]; !ok {
-			return fmt.Errorf("invalid hub ID: %s. %s", hubID, getAvailableHubsMessage())
+		err = verifyTokenSupply(cmd)
+		if err != nil {
+			return err
 		}
 		rollappID := args[0]
 		if !validateRollAppID(rollappID) {
@@ -28,12 +30,6 @@ func addFlags(cmd *cobra.Command) {
 		}
 		return nil
 	}
-}
-
-func getValidRollappIdMessage() string {
-	return "A valid RollApp ID should follow the format 'rollapp-name_EIP155_version', where 'rollapp-name' is made up of" +
-		" lowercase English letters, 'EIP155_version' is a 1 to 5 digit number representing the EIP155 rollapp ID, and '" +
-		"version' is a 1 to 5 digit number representing the version. For example: 'mars_9721_1'"
 }
 
 func getRollappBinaryPath(cmd *cobra.Command) string {
@@ -64,6 +60,11 @@ func GetInitConfig(initCmd *cobra.Command, args []string) (utils.RollappConfig, 
 		TokenSupply:   tokenSupply,
 	}, nil
 }
+func getValidRollappIdMessage() string {
+	return "A valid RollApp ID should follow the format 'rollapp-name_EIP155_version', where 'rollapp-name' is made up of" +
+		" lowercase English letters, 'EIP155_version' is a 1 to 5 digit number representing the EIP155 rollapp ID, and '" +
+		"version' is a 1 to 5 digit number representing the version. For example: 'mars_9721_1'"
+}
 
 func getAvailableHubsMessage() string {
 	return fmt.Sprintf("Acceptable values are '%s', '%s' or '%s'", TestnetHubID, StagingHubID, LocalHubID)
@@ -73,4 +74,38 @@ func validateRollAppID(id string) bool {
 	pattern := `^[a-z]+_[0-9]{1,5}_[0-9]{1,5}$`
 	r, _ := regexp.Compile(pattern)
 	return r.MatchString(id)
+}
+
+func verifyHubID(cmd *cobra.Command) error {
+	hubID, err := cmd.Flags().GetString(FlagNames.HubID)
+	if err != nil {
+		return err
+	}
+	if _, ok := Hubs[hubID]; !ok {
+		return fmt.Errorf("invalid hub ID: %s. %s", hubID, getAvailableHubsMessage())
+	}
+	return nil
+}
+
+func verifyTokenSupply(cmd *cobra.Command) error {
+	tokenSupplyStr, err := cmd.Flags().GetString(FlagNames.TokenSupply)
+	if err != nil {
+		return err
+	}
+
+	tokenSupply := new(big.Int)
+	_, ok := tokenSupply.SetString(tokenSupplyStr, 10)
+	if !ok {
+		return fmt.Errorf("invalid token supply: %s. Must be a valid integer", tokenSupplyStr)
+	}
+
+	ten := big.NewInt(10)
+	remainder := new(big.Int)
+	remainder.Mod(tokenSupply, ten)
+
+	if remainder.Cmp(big.NewInt(0)) != 0 {
+		return fmt.Errorf("invalid token supply: %s. Must be divisible by 10", tokenSupplyStr)
+	}
+
+	return nil
 }
