d324b1dace3e8d2a1494d0fa400eda8d2172921a
Michael Tsitrin <michael@dymension.xyz>
2023-06-28 15:52:45 +0300

    modified start values:

--- cmd/sequencer/start/start.go
+++ cmd/sequencer/start/start.go
@@ -79,15 +79,15 @@ func GetStartRollappCmd(rollappConfig utils.RollappConfig, lightNodeEndpoint str
 		"--dymint.block_batch_size", "500",
 		"--dymint.namespace_id", "000000000000ffff",
 		"--dymint.block_time", "0.2s",
-		"--dymint.batch_submit_max_time", "15s",
-		"--dymint.empty_blocks_max_time", "5s",
+		"--dymint.batch_submit_max_time", "100s",
+		"--dymint.empty_blocks_max_time", "10s",
 		"--dymint.settlement_config.rollapp_id", rollappConfig.RollappID,
 		"--dymint.settlement_config.node_address", rollappConfig.HubData.RPC_URL,
 		"--dymint.settlement_config.dym_account_name", consts.KeysIds.HubSequencer,
 		"--dymint.settlement_config.keyring_home_dir", hubKeysDir,
 		"--dymint.settlement_config.gas_prices", "0udym",
 		"--home", rollappConfigDir,
-		"--log_level", "debug",
+		"--log_level", "info",
 		"--max-log-size", "2000",
 	)
 
