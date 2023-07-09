584f5d9fc84beb0409086d19ea8e2294c6a61654
Michael Tsitrin <michael@dymension.xyz>
2023-07-09 09:28:50 +0300

    fixed PR

--- install.sh
+++ install.sh
@@ -52,7 +52,14 @@ display_go_installation_instructions() {
 
 # Download and extract the tar file to a temporary directory
 echo "$EMOJI Downloading roller..."
-if ! sudo curl -L "$TGZ_URL" --progress-bar | sudo tar -xz -C "/tmp/roller_tmp"; then
+if sudo curl -L "$TGZ_URL" --progress-bar | sudo tar -xz -C "/tmp/roller_tmp"; then
+    # Assuming that the tar file contains the lib folder and the roller and rollapp_evm binaries inside the roller_bins directory.
+    # Move binaries to their correct locations
+    echo "$EMOJI Installing roller..."
+    sudo mv "/tmp/roller_tmp/roller_bins/lib"/* "$INTERNAL_DIR"
+    sudo mv "/tmp/roller_tmp/roller_bins/roller" "$ROLLER_BIN_PATH"
+    sudo mv "/tmp/roller_tmp/roller_bins/rollapp_evm" "$ROLLAPP_EVM_PATH"  # move the rollapp_evm binary
+else
     # Curl failed. Fallback to clone and build.
     echo "$EMOJI Download failed. Cloning and building manually..."
     # Check prerequisites
@@ -108,21 +115,19 @@ if ! sudo curl -L "$TGZ_URL" --progress-bar | sudo tar -xz -C "/tmp/roller_tmp";
         fi
 
         for binary in ${BINARYNAME[i]}; do
+            if [ ! -x "$(command -v "$binary")" ]; then
+                echo "$EMOJI Couldn't find $binary in PATH. Aborting."; exit 1;
+            fi
             binary_path=$(which "$binary" 2> /dev/null)  # Redirect error output to /dev/null
             sudo cp "$binary_path" "$INTERNAL_DIR"
         done
     done
 
+
     # Move roller and rollapp_evm separately
     sudo mv "$INTERNAL_DIR/roller" "$ROLLER_BIN_PATH"
     sudo mv "$INTERNAL_DIR/rollapp_evm" "$ROLLAPP_EVM_PATH"
-else
-    # Assuming that the tar file contains the lib folder and the roller and rollapp_evm binaries inside the roller_bins directory.
-    # Move binaries to their correct locations
-    echo "$EMOJI Installing roller..."
-    sudo mv "/tmp/roller_tmp/roller_bins/lib"/* "$INTERNAL_DIR"
-    sudo mv "/tmp/roller_tmp/roller_bins/roller" "$ROLLER_BIN_PATH"
-    sudo mv "/tmp/roller_tmp/roller_bins/rollapp_evm" "$ROLLAPP_EVM_PATH"  # move the rollapp_evm binary
+
 fi
 
 
