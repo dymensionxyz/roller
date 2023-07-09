a56daa60d599098f19f89034fe1bc6b069a2f858
Itay Levy <itaylevy134@gmail.com>
2023-07-09 14:15:23 +0300

    Implemented rollapp height fetching

--- sequencer/status.go
+++ sequencer/status.go
@@ -13,35 +13,46 @@ type NodeInfo struct {
 	Network string `json:"network"`
 }
 
+type SyncInfo struct {
+	LatestBlockHeight string `json:"latest_block_height"`
+}
+
 type Result struct {
 	NodeInfo NodeInfo `json:"node_info"`
+	SyncInfo SyncInfo `json:"sync_info"`
 }
 
 type Response struct {
 	Result Result `json:"result"`
 }
 
-func IsSeqListen(rollappID string) bool {
+func getRollappHeight(rollappID string) string {
 	resp, err := http.Get(fmt.Sprintf("%s/status", consts.DefaultRollappRPC))
 	if err != nil {
-		return false
+		return "-1"
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return false
+		return "-1"
 	}
 	var response Response
 	if err := json.Unmarshal(body, &response); err != nil {
-		return false
+		return "-1"
 	}
-	return response.Result.NodeInfo.Network == rollappID
+	if response.Result.NodeInfo.Network == rollappID {
+		return response.Result.SyncInfo.LatestBlockHeight
+	} else {
+		return "-1"
+	}
 }
 
 func GetSequencerStatus(cfg config.RollappConfig) string {
 	// TODO: Make sure the sequencer status endpoint is being changed after block production is paused.
-	if IsSeqListen(cfg.RollappID) {
-		return "Active"
+	height := getRollappHeight(cfg.RollappID)
+	if height == "-1" {
+		return "Stopped, Restarting..."
+	} else {
+		return fmt.Sprintf("Active, Height: %s", height)
 	}
-	return "Stopped, Restarting..."
 }
