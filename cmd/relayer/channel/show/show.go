c3b23b50c9bb0069028fd4182a6cda8f6488a9e1
Itay Levy <itaylevy134@gmail.com>
2023-08-02 16:55:22 +0300

    Implemented roller channel show command

--- cmd/relayer/channel/show/show.go
+++ cmd/relayer/channel/show/show.go
@@ -2,6 +2,9 @@ package show
 
 import (
 	"fmt"
+	"github.com/dymensionxyz/roller/cmd/utils"
+	"github.com/dymensionxyz/roller/config"
+	"github.com/dymensionxyz/roller/relayer"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +13,18 @@ func Cmd() *cobra.Command {
 		Use:   "show",
 		Short: "Show the channel data of the relayer on the local machine.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("show channel")
+			home := cmd.Flag(utils.FlagNames.Home).Value.String()
+			rollappConfig, err := config.LoadConfigFromTOML(home)
+			utils.PrettifyErrorIfExists(err)
+			rly := relayer.NewRelayer(rollappConfig.Home, rollappConfig.RollappID, rollappConfig.HubData.ID)
+			srcChannel, dstChannel, err := rly.LoadChannels()
+			utils.PrettifyErrorIfExists(err)
+			if srcChannel == "" {
+				fmt.Println("💈 No channel has been created for the relayer yet.")
+			} else {
+				fmt.Printf("💈 Relayer Channels: src, %s <-> %s, dst",
+					srcChannel, dstChannel)
+			}
 		},
 	}
 	return cmd
