21b96f96937a75fde442745d6297ca3fd04aa16c
Itay Levy <itaylevy134@gmail.com>
2023-08-03 14:58:46 +0300

    Added the cors update to the next roller migrate

---
+++ cmd/migrate/v_0_1_6.go
@@ -0,0 +1,19 @@
+package migrate
+
+import (
+	"github.com/dymensionxyz/roller/config"
+	"github.com/dymensionxyz/roller/sequencer"
+)
+
+type VersionMigratorV016 struct{}
+
+func (v *VersionMigratorV016) ShouldMigrate(prevVersion VersionData) bool {
+	return prevVersion.Major < 1 && prevVersion.Minor < 2 && prevVersion.Patch < 6
+}
+
+func (v *VersionMigratorV016) PerformMigration(rlpCfg config.RollappConfig) error {
+	if err := sequencer.SetTMConfig(rlpCfg); err != nil {
+		return err
+	}
+	return UpdateRollerVersionInConfig(rlpCfg)
+}
