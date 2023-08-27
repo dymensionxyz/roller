cac3fc6623d7b154749e7543f34afa4aff824e40
Itay Levy <itaylevy134@gmail.com>
2023-08-27 13:26:25 +0300

    now the da is configurable

--- cmd/config/set/da.go
+++ cmd/config/set/da.go
@@ -13,25 +13,25 @@ func setDA(rlpCfg config.RollappConfig, value config.DAType) error {
 	if value == rlpCfg.DA {
 		return nil
 	}
-	switch value {
-	case config.Celestia:
-		if err := cleanDADir(rlpCfg); err != nil {
-			return err
+	supportedDas := []config.DAType{config.Celestia, config.Avail, config.Mock}
+	for _, da := range supportedDas {
+		if da == value {
+			return updateDaConfig(rlpCfg, value)
 		}
-		daManager := datalayer.NewDAManager(value, rlpCfg.Home)
-		if err := daManager.InitializeLightNodeConfig(); err != nil {
-			return err
-		}
-		rlpCfg.DA = value
-		return config.WriteConfigToTOML(rlpCfg)
-	case config.Avail:
-		return nil
-	case config.Mock:
-		return nil
-	default:
-		return fmt.Errorf("invalid DA type. Supported types are: %v", []string{string(config.Celestia),
-			string(config.Avail), string(config.Mock)})
 	}
+	return fmt.Errorf("invalid DA type. Supported types are: %v", supportedDas)
+}
+
+func updateDaConfig(rlpCfg config.RollappConfig, newDa config.DAType) error {
+	if err := cleanDADir(rlpCfg); err != nil {
+		return err
+	}
+	daManager := datalayer.NewDAManager(newDa, rlpCfg.Home)
+	if err := daManager.InitializeLightNodeConfig(); err != nil {
+		return err
+	}
+	rlpCfg.DA = newDa
+	return config.WriteConfigToTOML(rlpCfg)
 }
 
 func cleanDADir(cfg config.RollappConfig) error {
