98504f44ad1e3f5bea6ce3c55040d8252020bf4d
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2023-08-29 10:35:45 +0300

    fix: changed default empty blocks to 90s (#461)

--- sequencer/config.go
+++ sequencer/config.go
@@ -28,7 +28,7 @@ func SetDefaultDymintConfig(rlpCfg config.RollappConfig) error {
 	dymintCfg.Set("block_batch_size", "500")
 	dymintCfg.Set("block_time", "0.2s")
 	dymintCfg.Set("batch_submit_max_time", "100s")
-	dymintCfg.Set("empty_blocks_max_time", "3600s")
+	dymintCfg.Set("empty_blocks_max_time", "90s")
 	dymintCfg.Set("rollapp_id", rlpCfg.RollappID)
 	dymintCfg.Set("node_address", rlpCfg.HubData.RPC_URL)
 	dymintCfg.Set("dym_account_name", consts.KeysIds.HubSequencer)
