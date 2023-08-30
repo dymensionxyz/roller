cf7065cc38632742757817a7d51f6cf53aa27ff7
Ubuntu <ubuntu@ip-172-31-17-160.ec2.internal>
2023-08-30 15:23:35 +0000

    initializing docker successfully

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-
+#!/bin/bash
+random_number=$(( RANDOM % 100000 + 1 ))
+# will be passed in the orchestrator script
+IDENTIFIER="RAX"
+/usr/local/bin/roller config init loadtest_${random_number}-1 RAX --hub froopyland --da local
 echo "hello world"
