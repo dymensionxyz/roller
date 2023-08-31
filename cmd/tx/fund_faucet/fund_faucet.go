b10c99252bf34b9405eb38d6789c06653a985db4
Itay Levy <itaylevy134@gmail.com>
2023-08-31 18:30:21 +0300

    fixed PR comments

--- cmd/tx/fund_faucet/fund_faucet.go
+++ cmd/tx/fund_faucet/fund_faucet.go
@@ -9,12 +9,22 @@ import (
 	"github.com/dymensionxyz/roller/config"
 	"github.com/dymensionxyz/roller/relayer"
 	"github.com/spf13/cobra"
+	"math/big"
 	"os/exec"
 	"path/filepath"
 )
 
+var flagNames = struct {
+	tokenAmount string
+}{
+	tokenAmount: "token-amount",
+}
+
+const faucetAddr = "dym1g8sf7w4cz5gtupa6y62h3q6a4gjv37pgefnpt5"
+const faucetDefaultTokenAmount = "5000000"
+
 func Cmd() *cobra.Command {
-	versionCmd := &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "fund-faucet",
 		Short: "Fund the Dymension faucet with rollapp tokens",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -27,8 +37,15 @@ func Cmd() *cobra.Command {
 				utils.PrettifyErrorIfExists(errors.New("failed to load relayer channel. Please make sure that the rollapp is " +
 					"running on your local machine and a relayer channel has been established"))
 			}
+			faucetTokenAmountStr := cmd.Flag(flagNames.tokenAmount).Value.String()
+			faucetTokensAmount, success := new(big.Int).SetString(faucetTokenAmountStr, 10)
+			if !success {
+				utils.PrettifyErrorIfExists(fmt.Errorf("invalid faucet %s token amount", faucetTokenAmountStr))
+			}
+			actualFaucetAmount := faucetTokensAmount.Mul(faucetTokensAmount, new(big.Int).Exp(big.NewInt(10),
+				new(big.Int).SetUint64(uint64(rlpCfg.Decimals)), nil))
 			fundFaucetCmd := exec.Command(rlpCfg.RollappBinary, "tx", "ibc-transfer", "transfer", "transfer",
-				srcChannel, "dym1g8sf7w4cz5gtupa6y62h3q6a4gjv37pgefnpt5", "5000000000000000000000000"+rlpCfg.Denom, "--from",
+				srcChannel, faucetAddr, actualFaucetAmount.String()+rlpCfg.Denom, "--from",
 				consts.KeysIds.RollappSequencer, "--keyring-backend", "test", "--home", filepath.Join(rlpCfg.Home,
 					consts.ConfigDirName.Rollapp), "--broadcast-mode", "block", "-y")
 			stdout, err := utils.ExecBashCommandWithStdout(fundFaucetCmd)
@@ -38,5 +55,6 @@ func Cmd() *cobra.Command {
 			fmt.Println("💈 The Dymension faucet has been funded successfully!")
 		},
 	}
-	return versionCmd
+	cmd.Flags().StringP(flagNames.tokenAmount, "", faucetDefaultTokenAmount, "The amount of tokens to fund the faucet with")
+	return cmd
 }
