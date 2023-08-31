db1a45d004e522bccb7d38505e55669f4a989ea1
Itay Levy <itaylevy134@gmail.com>
2023-08-31 14:12:18 +0300

    keep workin

--- cmd/fund_faucet/fund_faucet.go
+++ cmd/fund_faucet/fund_faucet.go
@@ -1,8 +1,15 @@
 package fund_faucet
 
 import (
+	"errors"
 	"fmt"
+	"github.com/dymensionxyz/roller/cmd/consts"
+	"github.com/dymensionxyz/roller/cmd/utils"
+	"github.com/dymensionxyz/roller/config"
+	"github.com/dymensionxyz/roller/relayer"
 	"github.com/spf13/cobra"
+	"os/exec"
+	"path/filepath"
 )
 
 func Cmd() *cobra.Command {
@@ -10,7 +17,22 @@ func Cmd() *cobra.Command {
 		Use:   "fund-faucet",
 		Short: "Fund the Dymension faucet with rollapp tokens",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("funding it")
+			home := cmd.Flag(utils.FlagNames.Home).Value.String()
+			rlpCfg, err := config.LoadConfigFromTOML(home)
+			utils.PrettifyErrorIfExists(err)
+			rly := relayer.NewRelayer(rlpCfg.Home, rlpCfg.RollappID, rlpCfg.HubData.ID)
+			srcChannel, _, err := rly.LoadChannels()
+			if err != nil || srcChannel == "" {
+				utils.PrettifyErrorIfExists(errors.New("failed to load relayer channel. Please make sure that the rollapp is " +
+					"running on your local machine and a channel has been established"))
+			}
+			fundFuacetCmd := exec.Command(rlpCfg.RollappBinary, "tx", "ibc-transfer", "transfer", "transfer",
+				srcChannel, "dym1g8sf7w4cz5gtupa6y62h3q6a4gjv37pgefnpt5", "5000000000000000000000000"+rlpCfg.Denom, "--from",
+				consts.KeysIds.RollappSequencer, "--keyring-backend", "test", "--home", filepath.Join(rlpCfg.Home,
+					consts.ConfigDirName.Rollapp), "--broadcast-mode", "block")
+			stdout, err := utils.ExecBashCommandWithStdout(fundFuacetCmd)
+			utils.PrettifyErrorIfExists(err)
+			fmt.Println("💈 The Dymension faucet has been funded successfully!")
 		},
 	}
 	return versionCmd
