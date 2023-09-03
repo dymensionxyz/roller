e2e24373d478e7b31c3f1577b1f4deae8f428431
Itay <itaylevy134@gmail.com>
2023-09-03 12:28:08 +0300

    feat: Add a `roller tx fund-faucet` command (#479)

---
+++ cmd/tx/tx_utils/tx_utils.go
@@ -0,0 +1,27 @@
+package tx_utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
+func CheckTxStdOut(stdout bytes.Buffer) error {
+	var response Response
+
+	err := json.NewDecoder(&stdout).Decode(&response)
+	if err != nil {
+		return err
+	}
+
+	if strings.Contains(response.RawLog, "fail") || strings.Contains(response.RawLog, "error") {
+		return errors.New(response.RawLog)
+	}
+
+	return nil
+}
+
+type Response struct {
+	RawLog string `json:"raw_log"`
+}
