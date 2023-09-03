8aec89f284cc58468d8add46363c001af3be7e3b
Omri <omritoptix@gmail.com>
2023-09-03 16:11:41 +0300

    chore: update blocktime to 3600s (#485)

--- cmd/migrate/v_0_1_13.go
+++ cmd/migrate/v_0_1_13.go
@@ -3,6 +3,8 @@ package migrate
 import (
 	"github.com/dymensionxyz/roller/config"
 	"github.com/dymensionxyz/roller/relayer"
+	"github.com/dymensionxyz/roller/sequencer"
+	"github.com/dymensionxyz/roller/utils"
 )
 
 type VersionMigratorV0113 struct{}
@@ -12,6 +14,10 @@ func (v *VersionMigratorV0113) ShouldMigrate(prevVersion VersionData) bool {
 }
 
 func (v *VersionMigratorV0113) PerformMigration(rlpCfg config.RollappConfig) error {
+	// Update block time to 1 hour.
+	dymintTomlPath := sequencer.GetDymintFilePath(rlpCfg.Home)
+	utils.UpdateFieldInToml(dymintTomlPath, "empty_blocks_max_time", "3600s")
+	// Update relayer config path.
 	if err := relayer.DeletePath(rlpCfg, "hub-rollapp"); err != nil {
 		return err
 	}
