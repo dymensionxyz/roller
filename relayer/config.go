f0b0b1739ed56709c0c536391ab6dc71ce3ac2c3
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2023-09-05 12:41:46 +0300

    fix: roller extarcts wrong active channel (#498)

--- relayer/config.go
+++ relayer/config.go
@@ -2,13 +2,14 @@ package relayer
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
 	"github.com/dymensionxyz/roller/cmd/consts"
 	"github.com/dymensionxyz/roller/config"
 	"github.com/dymensionxyz/roller/utils"
 	"gopkg.in/yaml.v2"
-	"os"
-	"os/exec"
-	"path/filepath"
 )
 
 func CreatePath(rlpCfg config.RollappConfig) error {
@@ -55,8 +56,8 @@ func UpdateRlyConfigValue(rlpCfg config.RollappConfig, keyPath []string, newValu
 	return os.WriteFile(rlyConfigPath, newData, 0644)
 }
 
-func ReadRlyConfig(rlpCfg config.RollappConfig) (map[interface{}]interface{}, error) {
-	rlyConfigPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.Relayer, "config", "config.yaml")
+func ReadRlyConfig(homeDir string) (map[interface{}]interface{}, error) {
+	rlyConfigPath := filepath.Join(homeDir, consts.ConfigDirName.Relayer, "config", "config.yaml")
 	data, err := os.ReadFile(rlyConfigPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load %s: %v", rlyConfigPath, err)
@@ -69,8 +70,8 @@ func ReadRlyConfig(rlpCfg config.RollappConfig) (map[interface{}]interface{}, er
 	return rlyCfg, nil
 }
 
-func WriteRlyConfig(rlpCfg config.RollappConfig, rlyCfg map[interface{}]interface{}) error {
-	rlyConfigPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.Relayer, "config", "config.yaml")
+func WriteRlyConfig(homeDir string, rlyCfg map[interface{}]interface{}) error {
+	rlyConfigPath := filepath.Join(homeDir, consts.ConfigDirName.Relayer, "config", "config.yaml")
 	data, err := yaml.Marshal(rlyCfg)
 	if err != nil {
 		return fmt.Errorf("failed to marshal config: %v", err)
