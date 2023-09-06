d44e2f370e23cd134e6348b5af4bbb72e28f8d22
Itay <itaylevy134@gmail.com>
2023-09-06 10:47:23 +0300

    chore: Update `roller config set --help` to show the supported keys (#501)

--- cmd/config/set/set.go
+++ cmd/config/set/set.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dymensionxyz/roller/cmd/utils"
 	"github.com/dymensionxyz/roller/config"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 var keyUpdateFuncs = map[string]func(cfg config.RollappConfig, value string) error{
@@ -19,9 +20,10 @@ var keyUpdateFuncs = map[string]func(cfg config.RollappConfig, value string) err
 
 func Cmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "set <key> <value>",
-		Short: "Updates the specified key in all relevant places within the roller configuration files.",
-		Args:  cobra.ExactArgs(2),
+		Use: "set <key> <value>",
+		Short: fmt.Sprintf("Updates the specified key in all relevant places within the roller configuration files. "+
+			"The Supported keys are %s", strings.Join(getSupportedKeys(), ", ")),
+		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			home := cmd.Flag(utils.FlagNames.Home).Value.String()
 			rlpCfg, err := config.LoadConfigFromTOML(home)
@@ -33,7 +35,7 @@ func Cmd() *cobra.Command {
 			if updateFunc, exists := keyUpdateFuncs[key]; exists {
 				return updateFunc(rlpCfg, value)
 			}
-			return fmt.Errorf("invalid key. Supported keys are: %v", getSupportedKeys())
+			return fmt.Errorf("invalid key. Supported keys are: %v", strings.Join(getSupportedKeys(), ", "))
 		},
 	}
 	return cmd
