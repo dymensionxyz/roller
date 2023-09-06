c9037b68d4f627835a46e83e0ad39fc4b1693f95
Itay Levy <itaylevy134@gmail.com>
2023-09-06 12:42:39 +0300

    require migrate on roller sequencer start

--- version/version.go
+++ version/version.go
@@ -1,7 +1,13 @@
 package version
 
+import "strings"
+
 var (
-	BuildVersion = "<version>"
+	BuildVersion = "v0.1.13-rc03-afbhsdbh"
 	BuildTime    = "<build-time>"
 	BuildCommit  = "<build-commit>"
 )
+
+func TrimVersionStr(versionStr string) string {
+	return strings.Split(versionStr, "-")[0]
+}
