0bb850e37ab450ed685759578179581c82ff6492
omritoptix <omritoptix@gmail.com>
2023-12-04 18:06:29 +0100

    Changed relayer start command to use the actual rly start and not run periodic commands.

--- cmd/relayer/start/start.go
+++ cmd/relayer/start/start.go
@@ -1,7 +1,6 @@
 package start
 
 import (
-	"context"
 	"fmt"
 	"math/big"
 
@@ -58,16 +57,7 @@ func Start() *cobra.Command {
 				_, err := rly.CreateIBCChannel(override, logFileOption, seq)
 				utils.PrettifyErrorIfExists(err)
 			}
-
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			updateClientsCmd := rly.GetUpdateClientsCmd()
-			utils.RunCommandEvery(ctx, updateClientsCmd.Path, updateClientsCmd.Args[1:], 3600, logFileOption)
-			relayPacketsCmd := rly.GetRelayPacketsCmd()
-			utils.RunCommandEvery(ctx, relayPacketsCmd.Path, relayPacketsCmd.Args[1:], 5, logFileOption)
-			relayAcksCmd := rly.GetRelayAcksCmd()
-			utils.RunCommandEvery(ctx, relayAcksCmd.Path, relayAcksCmd.Args[1:], 5, logFileOption)
+			utils.RunBashCmdAsync(rly.GetStartCmd(), func() {}, func(errMessage string) string { return errMessage })
 			fmt.Printf("💈 The relayer is running successfully on you local machine! Channels: src, %s <-> %s, dst",
 				rly.SrcChannel, rly.DstChannel)
 
