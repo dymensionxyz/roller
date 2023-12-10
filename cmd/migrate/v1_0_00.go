ea5f00727c121bc233ca29f948d3d97c993e3b5d
omritoptix <omritoptix@gmail.com>
2023-12-10 15:18:18 +0100

    Fixed bug with relayer key migration.

--- cmd/migrate/v1_0_00.go
+++ cmd/migrate/v1_0_00.go
@@ -2,7 +2,9 @@ package migrate
 
 import (
 	"fmt"
+	"os/exec"
 
+	"github.com/dymensionxyz/roller/cmd/consts"
 	"github.com/dymensionxyz/roller/cmd/utils"
 	"github.com/dymensionxyz/roller/config"
 	"github.com/dymensionxyz/roller/relayer"
@@ -15,18 +17,22 @@ func (v *VersionMigratorV1000) ShouldMigrate(prevVersion VersionData) bool {
 }
 
 func (v *VersionMigratorV1000) PerformMigration(rlpCfg config.RollappConfig) error {
-	fmt.Println("💈 Updating relayer configuration to match new relayer...")
+	fmt.Println("💈 Migrating Rollapp key...")
+	migrateRollappKeyCmd := exec.Command(consts.Executables.RollappEVM, "keys", "migrate", "--home", rlpCfg.Home+"/relayer/keys/"+rlpCfg.RollappID, "--keyring-backend", "test")
+	out, err := utils.ExecBashCommandWithStdout(migrateRollappKeyCmd)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out.String())
+	fmt.Println("💈 Migrating Hub key...")
+	migrateHubKeyCmd := exec.Command(consts.Executables.Dymension, "keys", "migrate", "--home", rlpCfg.Home+"/relayer/keys/"+rlpCfg.HubData.ID, "--keyring-backend", "test")
+	out, err = utils.ExecBashCommandWithStdout(migrateHubKeyCmd)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out.String())
+	fmt.Println("💈 Updating relayer configuration to match new relayer key...")
 	if err := relayer.UpdateRlyConfigValue(rlpCfg, []string{"chains", rlpCfg.RollappID, "value", "extra-codecs"}, []string{"ethermint"}); err != nil {
-		return err
-	}
-	// Get relayer address in order to create a keys migration
-	fmt.Println("💈 Migrating relayer keys...")
-	_, err := utils.GetRelayerAddress(rlpCfg.Home, rlpCfg.HubData.ID)
-	if err != nil {
-		return err
-	}
-	_, err = utils.GetRelayerAddress(rlpCfg.Home, rlpCfg.RollappID)
-	if err != nil {
 		return err
 	}
 	return nil
