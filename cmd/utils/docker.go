c32af346075f2bc28f4b66f44ba66d15599d3d25
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-06-25 13:37:05 +0300

    chore: add docker helper

---
+++ cmd/utils/docker.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+)
+
+// TODO: this could be a generic docker container helper
+
+// CheckAndCreateMongoDBContainer function create a mongo db docker container that is utilized by
+// the eibc client to store fund information
+func CheckAndCreateMongoDBContainer(
+	ctx context.Context,
+	cli *client.Client,
+) error {
+	containerName := "eibc-mongodb"
+	imageName := "mongo:7.0"
+
+	portBindings := nat.PortMap{
+		"27017/tcp": []nat.PortBinding{
+			{
+				HostIP:   "0.0.0.0",
+				HostPort: "27017",
+			},
+		},
+	}
+
+	config := &container.Config{
+		Image: imageName,
+		ExposedPorts: nat.PortSet{
+			"27017/tcp": struct{}{},
+		},
+	}
+
+	hostConfig := &container.HostConfig{
+		PortBindings: portBindings,
+	}
+
+	containers, err := cli.ContainerList(ctx, container.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, c := range containers {
+		for _, name := range c.Names {
+			if strings.TrimPrefix(name, "/") == containerName {
+				fmt.Printf("Container %s already exists.\n", containerName)
+			}
+		}
+	}
+
+	resp, err := cli.ContainerCreate(ctx, config, hostConfig, nil, nil, containerName)
+	if err != nil {
+		return err
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		return err
+	}
+
+	fmt.Printf("Container %s created and started successfully\n", containerName)
+	return nil
+}
