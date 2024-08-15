179e49c9f3678427bffa9546e42e0e392140b151
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-08-15 06:38:29 +0300

    chore: disable commands

--- cmd/root.go
+++ cmd/root.go
@@ -5,17 +5,11 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/dymensionxyz/roller/cmd/config"
 	da_light_client "github.com/dymensionxyz/roller/cmd/da-light-client"
 	"github.com/dymensionxyz/roller/cmd/eibc"
-	"github.com/dymensionxyz/roller/cmd/keys"
-	"github.com/dymensionxyz/roller/cmd/migrate"
 	"github.com/dymensionxyz/roller/cmd/relayer"
 	"github.com/dymensionxyz/roller/cmd/rollapp"
-	"github.com/dymensionxyz/roller/cmd/run"
-	"github.com/dymensionxyz/roller/cmd/tx"
 	"github.com/dymensionxyz/roller/cmd/utils"
-	"github.com/dymensionxyz/roller/cmd/version"
 )
 
 var rootCmd = &cobra.Command{
@@ -34,16 +28,16 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.AddCommand(config.Cmd())
-	rootCmd.AddCommand(version.Cmd())
+	// rootCmd.AddCommand(config.Cmd())
+	// rootCmd.AddCommand(version.Cmd())
 	rootCmd.AddCommand(da_light_client.DALightClientCmd())
 	rootCmd.AddCommand(relayer.Cmd())
-	rootCmd.AddCommand(keys.Cmd())
-	rootCmd.AddCommand(run.Cmd())
+	// rootCmd.AddCommand(keys.Cmd())
+	// rootCmd.AddCommand(run.Cmd())
 	// rootCmd.AddCommand(services.Cmd())
-	rootCmd.AddCommand(migrate.Cmd())
-	rootCmd.AddCommand(tx.Cmd())
-	rootCmd.AddCommand(test())
+	// rootCmd.AddCommand(migrate.Cmd())
+	// rootCmd.AddCommand(tx.Cmd())
+	// rootCmd.AddCommand(test())
 	rootCmd.AddCommand(rollapp.Cmd())
 	rootCmd.AddCommand(eibc.Cmd())
 	utils.AddGlobalFlags(rootCmd)
