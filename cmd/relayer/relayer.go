12564c7b10eebaf3dbfeecb2b5b3969c6c663a03
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-08-15 07:17:41 +0300

    chore: add systemd service loaders

--- cmd/relayer/relayer.go
+++ cmd/relayer/relayer.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/dymensionxyz/roller/cmd/relayer/run"
 	"github.com/dymensionxyz/roller/cmd/relayer/status"
+	"github.com/dymensionxyz/roller/cmd/services"
+	loadservices "github.com/dymensionxyz/roller/cmd/services/load"
+	startservices "github.com/dymensionxyz/roller/cmd/services/start"
 )
 
 func Cmd() *cobra.Command {
@@ -15,5 +18,7 @@ func Cmd() *cobra.Command {
 	cmd.AddCommand(run.Cmd())
 	// cmd.AddCommand(start.Cmd())
 	cmd.AddCommand(status.Cmd())
+
+	cmd.AddCommand(services.Cmd(loadservices.RelayerCmd(), startservices.RelayerCmd()))
 	return cmd
 }
--- cmd/services/start/start.go
+++ cmd/services/start/start.go
@@ -47,3 +47,40 @@ func RollappCmd() *cobra.Command {
 	}
 	return cmd
 }
+
+func RelayerCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Loads the different rollapp services on the local machine",
+		Run: func(cmd *cobra.Command, args []string) {
+			if runtime.GOOS != "linux" {
+				pterm.Error.Printf(
+					"the %s commands are only available on linux machines",
+					pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
+						Sprintf("'services'"),
+				)
+
+				return
+			}
+			services := []string{"rollapp", "da-light-client"}
+			for _, service := range services {
+				err := servicemanager.StartSystemdService(fmt.Sprintf("%s.service", service))
+				if err != nil {
+					pterm.Error.Printf("failed to start %s systemd service: %v\n", service, err)
+					return
+				}
+			}
+			pterm.Success.Printf(
+				"💈 Services %s started successfully.\n",
+				strings.Join(services, ", "),
+			)
+			pterm.Info.Println("next steps:")
+			pterm.Info.Printf(
+				"run %s to set up IBC channels and start relaying packets\n",
+				pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
+					Sprintf("roller relayer run"),
+			)
+		},
+	}
+	return cmd
+}
