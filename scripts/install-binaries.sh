c4e41b564cbfa6a8fc6d0fb358a5954de21e9f66
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-08-27 19:51:05 +0400

    chore: update dymd commit version

--- scripts/install-binaries.sh
+++ scripts/install-binaries.sh
@@ -4,13 +4,13 @@ ROLLER_BINS_DIR="$BINS_DIR/roller_bins"
 
 ROLLAPP_EVN_VERSION="v2.2.1-rc01"
 DYMD_VERSION="main"
-DYMD_COMMIT="b9d863e6"
+DYMD_COMMIT="f42674ae"
 EIBC_VERSION="main"
 RLY_VERSION="v0.3.4-v2.5.2-relayer"
 CELESTIA_VERSION="v0.14.1"
 CELESTIA_APP_VERSION="v1.11.0"
 
-if [ -z "$BECH32_PREFIX" ]; then
+if [ "$BECH32_PREFIX" = "" ]; then
   echo "please provide BECH32_PREFIX of the RollApp before running this script"
   exit 1
 fi
@@ -41,7 +41,7 @@ if ! command -v "$BINS_DIR/dymd" &> /dev/null; then
     cd ../ && rm -rf dymension/
   elif [ "$DYMD_COMMIT" != "" ]; then
     git clone https://github.com/dymensionxyz/dymension.git && cd dymension || exit 1
-    git checkout $DYMD_COMMIT
+    git checkout "$DYMD_COMMIT"
     make build && sudo mv ./build/dymd "$BINS_DIR"
     cd ../ && rm -rf dymension/
   else
@@ -52,11 +52,10 @@ fi
 # eibc
 if ! command -v "$BINS_DIR/eibc-client" &> /dev/null; then
   cd ~/ && rm -rf eibc-client/
-  git clone https://github.com/dymensionxyz/eibc-client.git --branch $EIBC_VERSION && cd eibc-client || exit 1
+  git clone https://github.com/dymensionxyz/eibc-client.git --branch "$EIBC_VERSION" && cd eibc-client || exit 1
   make build && sudo mv ./build/eibc-client "$BINS_DIR"
   cd ../ && rm -rf eibc-client/
 fi
-
 
 # /usr/local/bin/roller_bins
 if [ ! -d "$ROLLER_BINS_DIR" ]; then
@@ -66,7 +65,7 @@ fi
 # celestia & cel-key
 if ! command -v "$ROLLER_BINS_DIR/celestia" &> /dev/null || ! command -v "$ROLLER_BINS_DIR/cel-key" &> /dev/null; then
   cd ~/ && rm -rf celestia-node
-  git clone https://github.com/celestiaorg/celestia-node.git --branch $CELESTIA_VERSION && cd celestia-node || exit 1
+  git clone https://github.com/celestiaorg/celestia-node.git --branch "$CELESTIA_VERSION" && cd celestia-node || exit 1
   if ! command -v "$ROLLER_BINS_DIR/celestia" &> /dev/null; then
     make build && sudo mv build/celestia "$ROLLER_BINS_DIR"
   fi
@@ -80,7 +79,7 @@ fi
 # celestia-app
 if ! command -v "$ROLLER_BINS_DIR/celestia-appd" &> /dev/null; then
   cd ~/ && rm -rf celestia-app/
-  git clone https://github.com/celestiaorg/celestia-app.git --branch $CELESTIA_APP_VERSION && cd celestia-app || exit 1
+  git clone https://github.com/celestiaorg/celestia-app.git --branch "$CELESTIA_APP_VERSION" && cd celestia-app || exit 1
   make build && sudo mv build/celestia-appd "$ROLLER_BINS_DIR"
   cd ../ && rm -rf celestia-app/
 fi
@@ -88,7 +87,7 @@ fi
 # rly
 if ! command -v "$ROLLER_BINS_DIR/rly" &> /dev/null; then
   cd ~/ && rm -rf go-relayer/
-  git clone https://github.com/dymensionxyz/go-relayer.git --branch $RLY_VERSION && cd go-relayer || exit 1
+  git clone https://github.com/dymensionxyz/go-relayer.git --branch "$RLY_VERSION" && cd go-relayer || exit 1
   make build && sudo mv build/rly "$ROLLER_BINS_DIR"
   cd ../ && rm -rf go-relayer/
 fi
