e4c5039bae7b0757c2cc6e654e706f06911597fb
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-08-27 21:46:25 +0400

    chore: merge main into branch

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -3,9 +3,8 @@ package version
 import (
 	"fmt"
 
+	versionutils "github.com/dymensionxyz/roller/version"
 	"github.com/spf13/cobra"
-
-	"github.com/dymensionxyz/roller/version"
 )
 
 func Cmd() *cobra.Command {
@@ -13,9 +12,9 @@ func Cmd() *cobra.Command {
 		Use:   "version",
 		Short: "Print the version of roller",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("💈 Roller version", version.BuildVersion)
-			fmt.Println("💈 Build time:", version.BuildTime)
-			fmt.Println("💈 Git commit:", version.BuildCommit)
+			fmt.Println("💈 Roller version", versionutils.BuildVersion)
+			fmt.Println("💈 Build time:", versionutils.BuildTime)
+			fmt.Println("💈 Git commit:", versionutils.BuildCommit)
 		},
 	}
 	return versionCmd
--- scripts/install-binaries.sh
+++ scripts/install-binaries.sh
@@ -2,7 +2,7 @@
 BINS_DIR="/usr/local/bin"
 ROLLER_BINS_DIR="$BINS_DIR/roller_bins"
 
-ROLLAPP_EVN_VERSION="v2.2.1-rc01"
+ROLLAPP_EVN_VERSION="main"
 DYMD_VERSION="main"
 DYMD_COMMIT="f42674ae"
 EIBC_VERSION="main"
@@ -15,79 +15,91 @@ if [ "$BECH32_PREFIX" = "" ]; then
   exit 1
 fi
 
-# /usr/local/bin
+install_or_update() {
+    local tool=$1
+    local current_version=$2
+    local target_version=$3
+    local repo_url=$4
+    local build_cmd=$5
+    local bin_name=$6
+    local bin_dir=$7
+    local extra_cmd=$8
+
+    if [ "$current_version" != "$target_version" ]; then
+        echo "Updating $tool from version $current_version to $target_version"
+        cd ~/ && rm -rf "$tool"/
+        git clone -q "$repo_url" --branch "$target_version" && cd "$tool" || exit 1
+        eval "$extra_cmd"
+        eval "$build_cmd" && sudo mv "$bin_name" "$bin_dir"
+        cd ../ && rm -rf "$tool"/
+    else
+        echo "$tool is already at the correct version $target_version"
+    fi
+}
+
 # roller
 if ! command -v "$BINS_DIR/roller" &> /dev/null; then
-  cd ~/ && rm -rf roller/
-  git clone https://github.com/dymensionxyz/roller.git && cd roller || exit 1
-  make build && sudo mv ./build/roller "$BINS_DIR"
-  cd ../ && rm -rf roller/
+    install_or_update "roller" "" "main" \
+        "https://github.com/dymensionxyz/roller.git" "make buill" "./build/roller" "$BINS_DIR"
 fi
 
 # rollapp-evm
-if ! command -v "$BINS_DIR/rollapp-evm" &> /dev/null; then
-  cd ~/ && rm -rf rollapp-evm/
-  git clone https://github.com/dymensionxyz/rollapp-evm.git --branch "$ROLLAPP_EVN_VERSION" && cd rollapp-evm || exit 1
-  make build && sudo mv ./build/rollapp-evm "$BINS_DIR"
-  cd ../ && rm -rf rollapp-evm/
-fi
+current_version=$("$BINS_DIR/rollapp-evm" version 2>/dev/null | cut -d ' ' -f 3 || echo "")
+install_or_update "rollapp-evm" "$current_version" "$ROLLAPP_EVN_VERSION" \
+    "https://github.com/dymensionxyz/rollapp-evm.git" "make build" "./build/rollapp-evm" "$BINS_DIR"
 
-#dymd
-if ! command -v "$BINS_DIR/dymd" &> /dev/null; then
-  cd ~/ && rm -rf dymension/
-  if [ "$DYMD_VERSION" != "" ]; then
-    git clone https://github.com/dymensionxyz/dymension.git --branch "$DYMD_VERSION" && cd dymension || exit 1
-    make build && sudo mv ./build/dymd "$BINS_DIR"
-    cd ../ && rm -rf dymension/
-  elif [ "$DYMD_COMMIT" != "" ]; then
-    git clone https://github.com/dymensionxyz/dymension.git && cd dymension || exit 1
-    git checkout "$DYMD_COMMIT"
-    make build && sudo mv ./build/dymd "$BINS_DIR"
-    cd ../ && rm -rf dymension/
-  else
-    exit 1
-  fi
+# dymd
+current_version=$("$BINS_DIR/dymd" version 2>/dev/null | cut -d ' ' -f 3 || echo "")
+if [ "$DYMD_VERSION" != "main" ]; then
+    install_or_update "dymension" "$current_version" "$DYMD_VERSION" \
+        "https://github.com/dymensionxyz/dymension.git" "make build" "./build/dymd" "$BINS_DIR"
+else
+    if [ -n "$DYMD_COMMIT" ]; then
+        echo "Installing dymd from main branch with specific commit $DYMD_COMMIT"
+        cd ~/ && rm -rf dymension/
+        git clone https://github.com/dymensionxyz/dymension.git && cd dymension || exit 1
+        git checkout $DYMD_COMMIT
+        make build && sudo mv ./build/dymd "$BINS_DIR"
+        cd ../ && rm -rf dymension/
+    else
+        echo "Error: DYMD_VERSION is set to main and DYMD_COMMIT is not specified"
+        exit 1
+    fi
 fi
 
 # eibc
-if ! command -v "$BINS_DIR/eibc-client" &> /dev/null; then
-  cd ~/ && rm -rf eibc-client/
-  git clone https://github.com/dymensionxyz/eibc-client.git --branch "$EIBC_VERSION" && cd eibc-client || exit 1
-  make build && sudo mv ./build/eibc-client "$BINS_DIR"
-  cd ../ && rm -rf eibc-client/
-fi
+current_version=$("$BINS_DIR/eibc-client" version 2>/dev/null | cut -d ' ' -f 3 || echo "")
+install_or_update "eibc-client" "$current_version" "$EIBC_VERSION" \
+    "https://github.com/dymensionxyz/eibc-client.git" "make build" "./build/eibc-client" "$BINS_DIR"
 
-# /usr/local/bin/roller_bins
+# Create ROLLER_BINS_DIR if it doesn't exist
 if [ ! -d "$ROLLER_BINS_DIR" ]; then
     sudo mkdir -p "$ROLLER_BINS_DIR"
 fi
 
 # celestia & cel-key
-if ! command -v "$ROLLER_BINS_DIR/celestia" &> /dev/null || ! command -v "$ROLLER_BINS_DIR/cel-key" &> /dev/null; then
-  cd ~/ && rm -rf celestia-node
-  git clone https://github.com/celestiaorg/celestia-node.git --branch "$CELESTIA_VERSION" && cd celestia-node || exit 1
-  if ! command -v "$ROLLER_BINS_DIR/celestia" &> /dev/null; then
+current_version=$("$ROLLER_BINS_DIR/celestia" version 2>/dev/null | awk '/Semantic version:/ {print $3}' || echo "")
+if [ "$current_version" != "$CELESTIA_VERSION" ] || ! command -v "$ROLLER_BINS_DIR/cel-key" &> /dev/null; then
+    echo "Installing/Updating Celestia from version $current_version to $CELESTIA_VERSION"
+    cd ~/ && rm -rf celestia-node
+    git clone https://github.com/celestiaorg/celestia-node.git --branch $CELESTIA_VERSION && cd celestia-node || exit 1
     make build && sudo mv build/celestia "$ROLLER_BINS_DIR"
-  fi
-
-  if ! command -v "$ROLLER_BINS_DIR/cel-key" &> /dev/null; then
     make cel-key && sudo mv cel-key "$ROLLER_BINS_DIR"
-  fi
-  cd ../ && rm -rf celestia-node
+    cd ../ && rm -rf celestia-node
+else
+    echo "Celestia is already at the correct version $CELESTIA_VERSION"
 fi
 
 # celestia-app
-if ! command -v "$ROLLER_BINS_DIR/celestia-appd" &> /dev/null; then
-  cd ~/ && rm -rf celestia-app/
-  git clone https://github.com/celestiaorg/celestia-app.git --branch "$CELESTIA_APP_VERSION" && cd celestia-app || exit 1
-  make build && sudo mv build/celestia-appd "$ROLLER_BINS_DIR"
-  cd ../ && rm -rf celestia-app/
-fi
+current_version=$("$ROLLER_BINS_DIR/celestia-appd" version)
+install_or_update "celestia-app" "v$current_version" "$CELESTIA_APP_VERSION" \
+    "https://github.com/celestiaorg/celestia-app.git" "make build" "build/celestia-appd" "$ROLLER_BINS_DIR"
 
 # rly
-if ! command -v "$ROLLER_BINS_DIR/rly" &> /dev/null; then
-  cd ~/ && rm -rf go-relayer/
-  git clone https://github.com/dymensionxyz/go-relayer.git --branch "$RLY_VERSION" && cd go-relayer || exit 1
-  make build && sudo mv build/rly "$ROLLER_BINS_DIR"
-  cd ../ && rm -rf go-relayer/
+current_version=$("$ROLLER_BINS_DIR/rly" version 2>/dev/null | awk '/^version:/ {print $2}' || echo "")
+if [ -z "$current_version" ] || [ "$current_version" != "${RLY_VERSION#v}" ]; then
+    install_or_update "go-relayer" "$current_version" "$RLY_VERSION" \
+        "https://github.com/dymensionxyz/go-relayer.git" "make build" "build/rly" "$ROLLER_BINS_DIR"
+else
+    echo "rly is already at the correct version $RLY_VERSION"
 fi
