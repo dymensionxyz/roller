f387d2d1d3a3084d6efdedbd15f3c3235ae11f27
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-09-09 11:53:28 +0400

    chore: update eibc fulfillment whitelist description

--- cmd/eibc/fulfill/denoms/set/set.go
+++ cmd/eibc/fulfill/denoms/set/set.go
@@ -18,9 +18,17 @@ import (
 
 func Cmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "set <ibc-denom-id> <value>",
-		Short: "Commands to manage the whitelist of RollApps to fulfill eibc orders for",
-		Args:  cobra.ExactArgs(2),
+		Use:   "set <ibc-denom-id> <fee-percentage>",
+		Short: "Commands to manage the whitelist of ibc-denoms to fulfill eibc orders for",
+		Long: `Commands to manage the whitelist of ibc-denoms to fulfill eibc orders for
+
+The fee-percentage is a float number between 0 and 100 which represents
+the minimal percentage of the order fee that you want to receive for fulfilling an order.
+Assume there's an eibc order for 100<token> with a fee of 3<token>,
+if the percentage is set to 4, this order will be ignored by your eibc client
+instance.
+`,
+		Args: cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			home, err := os.UserHomeDir()
 			if err != nil {
