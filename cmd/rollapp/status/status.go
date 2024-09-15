8c91c3e6e75a2bc7cb763925aec7adcd3d6f0e3b
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-09-15 15:59:25 +0400

    fix: file name

--- cmd/rollapp/status/status.go
+++ cmd/rollapp/status/status.go
@@ -5,13 +5,14 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/spf13/cobra"
+
 	"github.com/dymensionxyz/roller/cmd/consts"
 	"github.com/dymensionxyz/roller/cmd/rollapp/start"
 	"github.com/dymensionxyz/roller/cmd/utils"
 	"github.com/dymensionxyz/roller/sequencer"
 	"github.com/dymensionxyz/roller/utils/config/tomlconfig"
 	"github.com/dymensionxyz/roller/utils/errorhandling"
-	"github.com/spf13/cobra"
 )
 
 func Cmd() *cobra.Command {
@@ -29,7 +30,7 @@ func Cmd() *cobra.Command {
 			seq := sequencer.GetInstance(rollappConfig)
 			fmt.Println(seq.GetSequencerStatus())
 
-			pidFilePath := filepath.Join(home, consts.ConfigDirName.Rollapp, "pid")
+			pidFilePath := filepath.Join(home, consts.ConfigDirName.Rollapp, "rollapp.pid")
 			pid, err := os.ReadFile(pidFilePath)
 			if err != nil {
 				fmt.Println("failed to read pid file:", err)
