f992334d7c22082c9536bcf321219cf3c884a4e2
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-09-18 01:53:54 +0400

    feat: enable order polling

--- cmd/eibc/init/init.go
+++ cmd/eibc/init/init.go
@@ -97,8 +97,11 @@ func Cmd() *cobra.Command {
 
 			eibcConfigPath := filepath.Join(eibcHome, "config.yaml")
 			updates := map[string]interface{}{
-				"node_address":       rollerConfig.HubData.RPC_URL,
-				"whale.account_name": consts.KeysIds.Eibc,
+				"node_address":             rollerConfig.HubData.RPC_URL,
+				"whale.account_name":       consts.KeysIds.Eibc,
+				"order_poling.interval":    "25s",
+				"order_poling.indexer_url": "http://44.206.211.230:3000/",
+				"order_poling.enabled":     true,
 			}
 			err = yamlconfig.UpdateNestedYAML(eibcConfigPath, updates)
 			if err != nil {
