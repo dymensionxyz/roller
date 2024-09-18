94f27a708c45718ed222b4a62ecdee6265d6f568
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-09-18 11:12:02 +0400

    fix(eibc): improve error handling

--- cmd/eibc/start/start.go
+++ cmd/eibc/start/start.go
@@ -6,11 +6,13 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+
 	"github.com/dymensionxyz/roller/cmd/consts"
 	"github.com/dymensionxyz/roller/utils/bash"
 	eibcutils "github.com/dymensionxyz/roller/utils/eibc"
 	"github.com/dymensionxyz/roller/utils/filesystem"
-	"github.com/spf13/cobra"
 )
 
 func Cmd() *cobra.Command {
@@ -18,10 +20,12 @@ func Cmd() *cobra.Command {
 		Use:   "start",
 		Short: "Start the eibc client",
 		Run: func(cmd *cobra.Command, args []string) {
+			pterm.Info.Println("starting eibc client")
 			home, _ := os.UserHomeDir()
 			eibcHome := filepath.Join(home, consts.ConfigDirName.Eibc)
 			ok, err := filesystem.DirNotEmpty(eibcHome)
 			if err != nil {
+				pterm.Error.Println("failed to check eibc home directory:", err)
 				return
 			}
 
@@ -43,12 +47,15 @@ func Cmd() *cobra.Command {
 
 			err = eibcutils.CreateMongoDbContainer()
 			if err != nil {
+				pterm.Error.Println("failed to create mongodb container:", err)
 				return
 			}
+			pterm.Info.Println("created eibc mongodb container")
 
 			c := eibcutils.GetStartCmd()
 			err = bash.ExecCmdFollow(c)
 			if err != nil {
+				pterm.Error.Println("failed to create mongodb container:", err)
 				return
 			}
 		},
