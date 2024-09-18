e444b766cc95aa8df7c89755fa6dbbd43a4a95a5
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-09-18 23:32:24 +0400

    chore: remove deprecated code

--- cmd/rollapp/setup/setup.go
+++ cmd/rollapp/setup/setup.go
@@ -765,54 +765,6 @@ func Cmd() *cobra.Command {
 	return cmd
 }
 
-func printOutput(rlpCfg config.RollappConfig, cmd *exec.Cmd) {
-	seq := sequencer.GetInstance(rlpCfg)
-	pterm.DefaultSection.WithIndentCharacter("💈 ").
-		Println("The Rollapp sequencer is running on your local machine!")
-	fmt.Println("💈 Endpoints:")
-
-	fmt.Printf("EVM RPC: http://127.0.0.1:%v\n", seq.JsonRPCPort)
-	fmt.Printf("Node RPC: http://127.0.0.1:%v\n", seq.RPCPort)
-	fmt.Printf("Rest API: http://127.0.0.1:%v\n", seq.APIPort)
-
-	fmt.Println("💈 Log file path: ", LogPath)
-	fmt.Println("💈 Rollapp root dir: ", RollappDirPath)
-	fmt.Printf(
-		"💈 PID: %d (saved in %s)\n",
-		cmd.Process.Pid,
-		filepath.Join(rlpCfg.Home, "rollapputils.pid"),
-	)
-}
-
-func createPidFile(path string, cmd *exec.Cmd) error {
-	pidPath := filepath.Join(path, "rollapputils.pid")
-	file, err := os.Create(pidPath)
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return err
-	}
-	// nolint errcheck
-	defer file.Close()
-
-	pid := cmd.Process.Pid
-	_, err = file.WriteString(fmt.Sprintf("%d", pid))
-	if err != nil {
-		fmt.Println("Error writing to file:", err)
-		return err
-	}
-
-	return nil
-}
-
-func parseError(errMsg string) string {
-	lines := strings.Split(errMsg, "\n")
-	if len(lines) > 0 &&
-		lines[0] == "Error: failed to initialize database: resource temporarily unavailable" {
-		return "The Rollapp sequencer is already running on your local machine. Only one sequencer can run at any given time."
-	}
-	return errMsg
-}
-
 func populateSequencerMetadata(raCfg config.RollappConfig) error {
 	cd := dymensionseqtypes.ContactDetails{
 		Website:  "",
