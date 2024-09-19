471b6fd50ec90817ba8cf722d5d7a5bd797dfe68
VictorTrustyDev <me@victortrusty.dev>
2024-09-20 03:39:46 +0700

    cleanup code

--- management_web_service/index.go
+++ management_web_service/index.go
@@ -1,8 +1,6 @@
 package management_web_service
 
 import (
-	"fmt"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,24 +9,9 @@ func HandleWebIndex(c *gin.Context) {
 	w := wrapGin(c)
 	cfg := w.Config()
 
-	var whaleBalances sdk.Coins
-	var whaleBalancesStr []string
-	var err error
-	{
-		// example load account balances
-		whaleBalances, err = cfg.HubQueryClients.QueryAllBalances(cfg.WhaleAccount)
-		if err != nil {
-			panic(err)
-		}
-		for _, balance := range whaleBalances {
-			whaleBalancesStr = append(whaleBalancesStr, fmt.Sprintf("%s: %s", balance.Denom, balance.Amount.String()))
-		}
-	}
-
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"host":           c.Request.Host,
-		"ip":             cfg.IP,
-		"port":           cfg.Port,
-		"whale_balances": whaleBalances,
+		"host": c.Request.Host,
+		"ip":   cfg.IP,
+		"port": cfg.Port,
 	})
 }
