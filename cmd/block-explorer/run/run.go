39a987140f18355afa7f69fe5c2966c58e786ab1
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-09-20 11:00:50 +0400

    feat: run block explorer against any endpoint

--- cmd/block-explorer/run/run.go
+++ cmd/block-explorer/run/run.go
@@ -3,6 +3,7 @@ package run
 import (
 	"fmt"
 	"path/filepath"
+	"runtime"
 
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
@@ -11,6 +12,8 @@ import (
 	"github.com/dymensionxyz/roller/cmd/utils"
 	"github.com/dymensionxyz/roller/utils/blockexplorer"
 	"github.com/dymensionxyz/roller/utils/config/tomlconfig"
+	rollerfilesystemutils "github.com/dymensionxyz/roller/utils/filesystem"
+	"github.com/dymensionxyz/roller/utils/rollapp"
 )
 
 func Cmd() *cobra.Command {
@@ -23,6 +26,12 @@ func Cmd() *cobra.Command {
 			isFlagChanged := cmd.Flags().Changed("block-explorer-rpc-endpoint")
 			defaultBeRpcEndpoint, _ := cmd.Flags().GetString("block-explorer-rpc-endpoint")
 
+			hostAddress := "host.docker.internal"
+			if runtime.GOOS == "linux" {
+				hostAddress = "172.17.0.1" // Default Docker bridge network gateway
+			}
+			var raID string
+
 			var beRpcEndpoint string
 			if !isFlagChanged {
 				useDefaultEndpoint, _ := pterm.DefaultInteractiveConfirm.WithDefaultValue(false).
@@ -30,23 +39,53 @@ func Cmd() *cobra.Command {
 						fmt.Sprintf(
 							`'--block-explorer-rpc-endpoint' is not set,
 would you like to continue with the default endpoint (%s)?'
-press 'y' if you're running local node, press 'n' and provide the endpoint if you're running remote node`,
+if you're running a rollapp locally, press 'y',
+if you want to run block explorer for a rollapp on a different host, press 'n' and provide the endpoint and RollApp ID`,
 							defaultBeRpcEndpoint,
 						),
 					).
 					Show()
 				if useDefaultEndpoint {
-					beRpcEndpoint = defaultBeRpcEndpoint
+					beRpcEndpoint = fmt.Sprintf("http://%s:11100", hostAddress)
+
+					err := rollerfilesystemutils.UpdateHostsFile(
+						"127.0.0.1",
+						"host.docker.internal",
+					)
+					if err != nil {
+						pterm.Error.Println("failed to update hosts file", err)
+						return
+					}
+
+					rollerData, err := tomlconfig.LoadRollerConfig(home)
+					if err != nil {
+						pterm.Error.Println("failed to load roller config file", err)
+						return
+					}
+
+					raID = rollerData.RollappID
 				} else {
-					newBeRpcEndpoint, _ := pterm.DefaultInteractiveTextInput.Show()
+					newBeRpcEndpoint, _ := pterm.DefaultInteractiveTextInput.WithDefaultText(
+						"provide block explorer json rpc endpoint (running on port 11100 by default):",
+					).Show()
+					if newBeRpcEndpoint == "" {
+						pterm.Error.Println("invalid endpoint")
+						return
+					}
+
+					raIDInput, _ := pterm.DefaultInteractiveTextInput.WithDefaultText(
+						"provide a rollapp ID that you want to run the node for",
+					).Show()
+
+					_, err := rollapp.ValidateChainID(raIDInput)
+					if err != nil {
+						pterm.Error.Println("invalid rollapp ID", err)
+					}
+
+					raID = raIDInput
+
 					beRpcEndpoint = newBeRpcEndpoint
 				}
-			}
-
-			rollerData, err := tomlconfig.LoadRollerConfig(home)
-			if err != nil {
-				pterm.Error.Println("failed to load roller config file", err)
-				return
 			}
 
 			beChainConfigPath := filepath.Join(
@@ -56,10 +95,10 @@ press 'y' if you're running local node, press 'n' and provide the endpoint if yo
 				"chains.yaml",
 			)
 			beChainConfig := blockexplorer.GenerateChainsYAML(
-				rollerData.RollappID,
+				raID,
 				beRpcEndpoint,
 			)
-			err = blockexplorer.WriteChainsYAML(beChainConfigPath, beChainConfig)
+			err := blockexplorer.WriteChainsYAML(beChainConfigPath, beChainConfig)
 			if err != nil {
 				pterm.Error.Println("failed to generate block explorer config", err)
 			}
