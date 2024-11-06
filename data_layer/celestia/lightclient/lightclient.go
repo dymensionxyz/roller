63ddc6462194dda8bb520c49a4a6729dd460d2c5
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-11-06 14:47:27 +0700

    chore: debug

--- data_layer/celestia/lightclient/lightclient.go
+++ data_layer/celestia/lightclient/lightclient.go
@@ -30,7 +30,7 @@ func Initialize(env string, rollerData roller.RollappConfig) (*keys.KeyInfo, err
 		raID := rollerData.RollappID
 		kb := rollerData.KeyringBackend
 
-		fmt.Println(kb)
+		fmt.Println("kb:", kb)
 		if kb == consts.SupportedKeyringBackends.OS {
 			daSpinner.UpdateText("creating keyring passphrase file")
 			err := keys.CreateDaOsKeyringPswFile(rollerData.Home)
