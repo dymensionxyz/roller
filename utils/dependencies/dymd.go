635f8f86e711d92db8aaf3a2e170d24326585eab
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-11-19 16:48:57 +0700

    chore: update dymd version

--- utils/dependencies/dymd.go
+++ utils/dependencies/dymd.go
@@ -97,7 +97,7 @@ func DefaultDymdDependency() types.Dependency {
 		RepositoryOwner: "dymensionxyz",
 		RepositoryName:  "dymension",
 		RepositoryUrl:   "https://github.com/artemijspavlovs/dymension",
-		Release:         "v3.1.0-pg10",
+		Release:         "v3.1.0-mig01",
 		Binaries: []types.BinaryPathPair{
 			{
 				Binary:            "dymd",
