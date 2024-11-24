56dcd1a13ff0a96d1092a0e741d22764e18bc037
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-11-24 18:23:34 +0700

    fix: add spinner when restarting services during ibc connection creation

--- utils/dymint/dymint.go
+++ utils/dymint/dymint.go
@@ -131,6 +131,7 @@ func UpdateDymintConfigForIBC(home string, t string, forceUpdate bool) error {
 			return err
 		}
 
+		spin, _ := pterm.DefaultSpinner.Start("restarting rollapp")
 		if runtime.GOOS == "linux" {
 			err := servicemanager.RestartSystemdService("rollapp")
 			if err != nil {
@@ -147,7 +148,7 @@ func UpdateDymintConfigForIBC(home string, t string, forceUpdate bool) error {
 					", only linux and darwin are supported",
 			)
 		}
-
+		spin.Success("restart successful")
 	} else {
 		pterm.Info.Println("block time settings already up to date")
 		pterm.Info.Println("restarting rollapp process to ensure correct block time is applied")
