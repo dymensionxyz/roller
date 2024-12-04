2ff5e501375f510b5d3244e8bdce524986a05f1f
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-12-04 18:48:46 +0700

    feat: add the ability to disable health agent through config

--- cmd/rollapp/start/start.go
+++ cmd/rollapp/start/start.go
@@ -92,7 +92,7 @@ Consider using 'services' if you want to run a 'systemd'(unix) or 'launchd'(mac)
 
 			rollerLogger := logging.GetRollerLogger(rollappConfig.Home)
 
-			if rollappConfig.HubData.ID != "mock" {
+			if rollappConfig.HubData.ID != "mock" && rollappConfig.HealthAgent.Enabled {
 				go healthagent.Start(home, rollerLogger)
 			}
 
