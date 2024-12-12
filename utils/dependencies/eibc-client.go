976aa541b21896b9e5e856620b82fcd9ee39d135
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-12-12 18:21:00 +0700

    fix: get the default eibc client version from remote

--- utils/dependencies/eibc-client.go
+++ utils/dependencies/eibc-client.go
@@ -1,22 +1,27 @@
 package dependencies
 
 import (
+	"github.com/pterm/pterm"
+
 	"github.com/dymensionxyz/roller/cmd/consts"
 	"github.com/dymensionxyz/roller/utils/dependencies/types"
-)
-
-const (
-	DefaultEibcClientVersion = "v1.0.0-alpha-rc03"
+	"github.com/dymensionxyz/roller/utils/firebase"
 )
 
 func DefaultEibcClientPrebuiltDependencies() map[string]types.Dependency {
+	bvi, err := firebase.GetDependencyVersions()
+	if err != nil {
+		pterm.Error.Println("failed to fetch binary versions: ", err)
+		return nil
+	}
+
 	return map[string]types.Dependency{
 		"eibc-client": {
 			DependencyName:  "eibc-client",
 			RepositoryOwner: "dymensionxyz",
 			RepositoryName:  "eibc-client",
 			RepositoryUrl:   "https://github.com/dymensionxyz/eibc-client",
-			Release:         DefaultEibcClientVersion,
+			Release:         bvi.EibcClient,
 			Binaries: []types.BinaryPathPair{
 				{
 					Binary:            "eibc-client",
