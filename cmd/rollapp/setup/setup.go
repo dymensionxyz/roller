6e2342428cafa2145134116ed4a8c66c22fa836d
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-12-18 18:51:44 +0700

    chore: debug

--- cmd/rollapp/setup/setup.go
+++ cmd/rollapp/setup/setup.go
@@ -824,7 +824,7 @@ func populateSequencerMetadata(raCfg roller.RollappConfig) error {
 	}
 
 	var denom string
-	fmt.Println(as.RollappParams.Params.MinGasPrices)
+	fmt.Println(as.RollappParams.Params.MinGasPrices[0].Amount)
 	if len(as.RollappParams.Params.MinGasPrices) == 1 {
 		defaultGasPrice = cosmossdkmath.Int(as.RollappParams.Params.MinGasPrices[0].Amount)
 		denom = as.RollappParams.Params.MinGasPrices[0].Denom
