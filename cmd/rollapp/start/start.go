5741e93dd2dbe3f322f359545ee6d02d2b38489d
Michael Tsitrin <michael@dymension.xyz>
2024-12-18 14:40:20 +0200

    skip migration check

--- cmd/rollapp/start/start.go
+++ cmd/rollapp/start/start.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"runtime"
 	"slices"
 	"strings"
 
@@ -15,16 +14,13 @@ import (
 
 	initconfig "github.com/dymensionxyz/roller/cmd/config/init"
 	"github.com/dymensionxyz/roller/cmd/consts"
-	datalayer "github.com/dymensionxyz/roller/data_layer"
 	"github.com/dymensionxyz/roller/sequencer"
 	"github.com/dymensionxyz/roller/utils/bash"
 	"github.com/dymensionxyz/roller/utils/filesystem"
 	"github.com/dymensionxyz/roller/utils/healthagent"
 	"github.com/dymensionxyz/roller/utils/logging"
-	"github.com/dymensionxyz/roller/utils/migrations"
 	"github.com/dymensionxyz/roller/utils/roller"
 	sequencerutils "github.com/dymensionxyz/roller/utils/sequencer"
-	"github.com/dymensionxyz/roller/utils/upgrades"
 )
 
 // var OneDaySequencePrice = big.NewInt(1)
@@ -68,22 +64,22 @@ Consider using 'services' if you want to run a 'systemd'(unix) or 'launchd'(mac)
 				return
 			}
 
-			if rollappConfig.HubData.ID != consts.MockHubID {
-				raUpgrade, err := upgrades.NewRollappUpgrade(string(rollappConfig.RollappVMType))
-				if err != nil {
-					pterm.Error.Println("failed to check rollapp version equality: ", err)
-				}
+			// if rollappConfig.HubData.ID != consts.MockHubID {
+			// 	raUpgrade, err := upgrades.NewRollappUpgrade(string(rollappConfig.RollappVMType))
+			// 	if err != nil {
+			// 		pterm.Error.Println("failed to check rollapp version equality: ", err)
+			// 	}
 
-				err = migrations.RequireRollappMigrateIfNeeded(
-					raUpgrade.CurrentVersionCommit[:6],
-					rollappConfig.RollappBinaryVersion[:6],
-					string(rollappConfig.RollappVMType),
-				)
-				if err != nil {
-					pterm.Error.Println(err)
-					return
-				}
-			}
+			// 	err = migrations.RequireRollappMigrateIfNeeded(
+			// 		raUpgrade.CurrentVersionCommit[:6],
+			// 		rollappConfig.RollappBinaryVersion[:6],
+			// 		string(rollappConfig.RollappVMType),
+			// 	)
+			// 	if err != nil {
+			// 		pterm.Error.Println(err)
+			// 		return
+			// 	}
+			// }
 
 			err = sequencerutils.CheckBalance(rollappConfig)
 			if err != nil {
@@ -169,101 +165,6 @@ Consider using 'services' if you want to run a 'systemd'(unix) or 'launchd'(mac)
 	cmd.Flags().String("log-level", "debug", "pass the log level to the rollapp")
 
 	return cmd
-}
-
-func PrintOutput(
-	rlpCfg roller.RollappConfig,
-	withBalance,
-	withEndpoints,
-	withProcessInfo,
-	isHealthy bool,
-	dymintNodeID string,
-) {
-	rollappDirPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.Rollapp)
-
-	seq := sequencer.GetInstance(rlpCfg)
-
-	var msg string
-	if isHealthy {
-		msg = pterm.DefaultBasicText.WithStyle(
-			pterm.
-				FgGreen.ToStyle(),
-		).Sprintf("💈 The Rollapp %s is running on your local machine!", rlpCfg.NodeType)
-	} else {
-		msg = pterm.DefaultBasicText.WithStyle(
-			pterm.
-				FgRed.ToStyle(),
-		).Sprintf(
-			"❗ The Rollapp %s is in unhealthy state. Please check the logs for more information.",
-			rlpCfg.NodeType,
-		)
-	}
-
-	fmt.Println(msg)
-	pterm.Println()
-	fmt.Printf(
-		"💈 RollApp ID: %s\n", pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
-			Sprint(rlpCfg.RollappID),
-	)
-	fmt.Printf(
-		"💈 Keyring Backend: %s\n", pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
-			Sprint(rlpCfg.KeyringBackend),
-	)
-
-	fmt.Printf(
-		"💈 Node ID: %s\n", pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
-			Sprint(dymintNodeID),
-	)
-
-	if withEndpoints {
-		pterm.DefaultSection.WithIndentCharacter("💈").
-			Println("Endpoints:")
-		if rlpCfg.RollappVMType == "evm" {
-			fmt.Printf("EVM RPC: http://0.0.0.0:%v\n", seq.JsonRPCPort)
-		}
-		fmt.Printf("Node RPC: http://0.0.0.0:%v\n", seq.RPCPort)
-		fmt.Printf("Rest API: http://0.0.0.0:%v\n", seq.APIPort)
-	}
-
-	pterm.DefaultSection.WithIndentCharacter("💈").
-		Println("Filesystem Paths:")
-	fmt.Println("Rollapp root dir: ", rollappDirPath)
-
-	if withProcessInfo {
-		pterm.DefaultSection.WithIndentCharacter("💈").
-			Println("Process Info:")
-		fmt.Println("OS:", runtime.GOOS)
-		fmt.Println("Architecture:", runtime.GOARCH)
-	}
-
-	if isHealthy {
-		seqAddrData, err := sequencerutils.GetSequencerData(rlpCfg)
-		daManager := datalayer.NewDAManager(consts.Celestia, rlpCfg.Home, rlpCfg.KeyringBackend)
-		celAddrData, errCel := daManager.GetDAAccData(rlpCfg)
-		if err != nil {
-			return
-		}
-
-		if err != nil {
-			return
-		}
-		pterm.DefaultSection.WithIndentCharacter("💈").
-			Println("Wallet Info:")
-		fmt.Println("Sequencer Address:", seqAddrData[0].Address)
-		if withBalance && rlpCfg.NodeType == "sequencer" {
-			fmt.Println("Sequencer Balance:", seqAddrData[0].Balance.String())
-		}
-
-		if errCel != nil {
-			pterm.Error.Println("failed to retrieve DA address")
-			return
-		}
-
-		fmt.Println("Da Address:", celAddrData[0].Address)
-		if withBalance && rlpCfg.NodeType == "sequencer" && rlpCfg.HubData.ID != "mock" {
-			fmt.Println("Da Balance:", celAddrData[0].Balance.String())
-		}
-	}
 }
 
 func printDaOutput() {
--- cmd/rollapp/status/status.go
+++ cmd/rollapp/status/status.go
@@ -2,11 +2,15 @@ package status
 
 import (
 	"fmt"
+	"path/filepath"
+	"runtime"
 
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 
 	initconfig "github.com/dymensionxyz/roller/cmd/config/init"
-	"github.com/dymensionxyz/roller/cmd/rollapp/start"
+	"github.com/dymensionxyz/roller/cmd/consts"
+	"github.com/dymensionxyz/roller/sequencer"
 	"github.com/dymensionxyz/roller/utils/dymint"
 	"github.com/dymensionxyz/roller/utils/healthagent"
 	"github.com/dymensionxyz/roller/utils/roller"
@@ -33,13 +37,108 @@ func Cmd() *cobra.Command {
 			ok, msg := healthagent.IsEndpointHealthy("http://localhost:26657/health")
 			if !ok {
 				// TODO: use options pattern, this is ugly af
-				start.PrintOutput(rollerConfig, true, false, true, false, nodeID)
+				PrintOutput(rollerConfig, true, false, true, false, nodeID)
 				fmt.Println("Unhealthy Message: ", msg)
 				return
 			}
 
-			start.PrintOutput(rollerConfig, true, true, true, true, nodeID)
+			PrintOutput(rollerConfig, true, true, true, true, nodeID)
 		},
 	}
 	return cmd
+}
+
+func PrintOutput(
+	rlpCfg roller.RollappConfig,
+	withBalance,
+	withEndpoints,
+	withProcessInfo,
+	isHealthy bool,
+	dymintNodeID string,
+) {
+	rollappDirPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.Rollapp)
+
+	seq := sequencer.GetInstance(rlpCfg)
+
+	var msg string
+	if isHealthy {
+		msg = pterm.DefaultBasicText.WithStyle(
+			pterm.
+				FgGreen.ToStyle(),
+		).Sprintf("💈 The Rollapp %s is running on your local machine!", rlpCfg.NodeType)
+	} else {
+		msg = pterm.DefaultBasicText.WithStyle(
+			pterm.
+				FgRed.ToStyle(),
+		).Sprintf(
+			"❗ The Rollapp %s is in unhealthy state. Please check the logs for more information.",
+			rlpCfg.NodeType,
+		)
+	}
+
+	fmt.Println(msg)
+	pterm.Println()
+	fmt.Printf(
+		"💈 RollApp ID: %s\n", pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
+			Sprint(rlpCfg.RollappID),
+	)
+	fmt.Printf(
+		"💈 Keyring Backend: %s\n", pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
+			Sprint(rlpCfg.KeyringBackend),
+	)
+
+	fmt.Printf(
+		"💈 Node ID: %s\n", pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
+			Sprint(dymintNodeID),
+	)
+
+	if withEndpoints {
+		pterm.DefaultSection.WithIndentCharacter("💈").
+			Println("Endpoints:")
+		if rlpCfg.RollappVMType == "evm" {
+			fmt.Printf("EVM RPC: http://0.0.0.0:%v\n", seq.JsonRPCPort)
+		}
+		fmt.Printf("Node RPC: http://0.0.0.0:%v\n", seq.RPCPort)
+		fmt.Printf("Rest API: http://0.0.0.0:%v\n", seq.APIPort)
+	}
+
+	pterm.DefaultSection.WithIndentCharacter("💈").
+		Println("Filesystem Paths:")
+	fmt.Println("Rollapp root dir: ", rollappDirPath)
+
+	if withProcessInfo {
+		pterm.DefaultSection.WithIndentCharacter("💈").
+			Println("Process Info:")
+		fmt.Println("OS:", runtime.GOOS)
+		fmt.Println("Architecture:", runtime.GOARCH)
+	}
+
+	if isHealthy {
+		seqAddrData, err := sequencerutils.GetSequencerData(rlpCfg)
+		daManager := datalayer.NewDAManager(consts.Celestia, rlpCfg.Home, rlpCfg.KeyringBackend)
+		celAddrData, errCel := daManager.GetDAAccData(rlpCfg)
+		if err != nil {
+			return
+		}
+
+		if err != nil {
+			return
+		}
+		pterm.DefaultSection.WithIndentCharacter("💈").
+			Println("Wallet Info:")
+		fmt.Println("Sequencer Address:", seqAddrData[0].Address)
+		if withBalance && rlpCfg.NodeType == "sequencer" {
+			fmt.Println("Sequencer Balance:", seqAddrData[0].Balance.String())
+		}
+
+		if errCel != nil {
+			pterm.Error.Println("failed to retrieve DA address")
+			return
+		}
+
+		fmt.Println("Da Address:", celAddrData[0].Address)
+		if withBalance && rlpCfg.NodeType == "sequencer" && rlpCfg.HubData.ID != "mock" {
+			fmt.Println("Da Balance:", celAddrData[0].Balance.String())
+		}
+	}
 }
