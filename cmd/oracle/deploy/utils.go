f150020d399c5c6ccde0f2c904cffbd7cfe51de3
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2025-01-03 06:49:14 +0700

    fix: download oracle from remote

--- cmd/oracle/deploy/utils.go
+++ cmd/oracle/deploy/utils.go
@@ -3,6 +3,9 @@ package deploy
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
 
@@ -50,6 +53,12 @@ func (o *Oracle) Deploy(rollerData roller.RollappConfig) error {
 	o.Address = addr[0].Address
 	pterm.Info.Printfln("using oracle key: %s", o.Address)
 
+	pterm.Info.Println("downloading oracle contract...")
+	if err := o.DownloadContractCode(); err != nil {
+		return fmt.Errorf("failed to download contract: %v", err)
+	}
+	pterm.Success.Println("contract downloaded successfully")
+
 	if err := o.StoreContract(rollerData); err != nil {
 		return fmt.Errorf("failed to store contract: %v", err)
 	}
@@ -68,25 +77,40 @@ func (o *Oracle) Deploy(rollerData roller.RollappConfig) error {
 	return nil
 }
 
-func (o *Oracle) DownloadContractCode() {
-	pterm.Error.Println("unimplemented")
-}
+func (o *Oracle) DownloadContractCode() error {
+	contractURL := "https://storage.googleapis.com/dymension-roller/centralized_oracle.wasm"
+	contractPath := filepath.Join(o.ConfigDirPath, "centralized_oracle.wasm")
 
-func (o *Oracle) GetStoreWasmContractCmd() *exec.Cmd {
-	return exec.Command(
-		consts.Executables.RollappEVM,
-		"tx",
-		"store",
-		filepath.Join(o.ConfigDirPath, "oracle.wasm"),
-		"--from",
-		o.Key,
-		"--chain-id",
-		"dymension_100-1",
-		"--output",
-		"json",
-		"--home",
-		"/Users/artemijspavlovs/.roller/rollapp/config",
-	)
+	// Create config directory if it doesn't exist
+	if err := os.MkdirAll(o.ConfigDirPath, 0o755); err != nil {
+		return fmt.Errorf("failed to create config directory: %v", err)
+	}
+
+	// Create the file
+	out, err := os.Create(contractPath)
+	if err != nil {
+		return fmt.Errorf("failed to create contract file: %v", err)
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(contractURL)
+	if err != nil {
+		return fmt.Errorf("failed to download contract: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download contract, status: %s", resp.Status)
+	}
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save contract: %v", err)
+	}
+
+	return nil
 }
 
 func generateRaOracleKeys(home string, rollerData roller.RollappConfig) ([]keys.KeyInfo, error) {
