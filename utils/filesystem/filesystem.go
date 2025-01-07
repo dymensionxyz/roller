e8e1213f8bde160ed6d0054f584c376a7234c7c0
PrathyushaLakkireddy <prathyusha@vitwit.com>
2025-01-07 16:08:04 +0530

    address comments

--- utils/filesystem/filesystem.go
+++ utils/filesystem/filesystem.go
@@ -1,7 +1,6 @@
 package filesystem
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -118,6 +117,7 @@ func ExpandHomePath(path string) (string, error) {
 	return path, nil
 }
 
+// TODO: download the file in chunks if possible
 func DownloadFile(url, fp string) error {
 	err := os.MkdirAll(filepath.Dir(fp), 0o755)
 	if err != nil {
@@ -125,50 +125,26 @@ func DownloadFile(url, fp string) error {
 	}
 
 	spinner, _ := pterm.DefaultSpinner.
-		Start("Accessing ", filepath.Base(fp))
+		Start("Downloading ", filepath.Base(fp))
 	fmt.Println()
 
-	// Check if the URL is a local file path
-	if _, err := os.Stat(url); err == nil {
-		// The URL is a local file; copy it to the target location
-		sourceFile, err := os.Open(url)
-		if err != nil {
-			spinner.Fail("failed to access local file: ", err)
-			return err
-		}
-		defer sourceFile.Close()
-
-		out, err := os.Create(fp)
-		if err != nil {
-			spinner.Fail("failed to create output file: ", err)
-			return err
-		}
-		defer out.Close()
-
-		// Copy the local file to the target path
-		_, err = io.Copy(out, sourceFile)
-		if err != nil {
-			spinner.Fail("failed to copy local file: ", err)
-			return err
-		}
-		spinner.Success(fmt.Sprintf("Successfully accessed the local file %s", filepath.Base(fp)))
-		return nil
-	}
-
-	// If not a local file, proceed with downloading from the URL
+	// nolint:gosec
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode != http.StatusOK {
+		// nolint:errcheck,gosec
 		resp.Body.Close()
 		spinner.Fail("failed to download file: ", err)
 		return err
 	}
+	// nolint:errcheck
 	defer resp.Body.Close()
 
 	out, err := os.Create(fp)
 	if err != nil {
-		spinner.Fail("failed to create output file: ", err)
+		spinner.Fail("failed to download file: ", err)
 		return err
 	}
+	// nolint:errcheck
 	defer out.Close()
 
 	spinner.Success(fmt.Sprintf("Successfully downloaded the %s", filepath.Base(fp)))
@@ -177,13 +153,6 @@ func DownloadFile(url, fp string) error {
 		return err
 	}
 	return nil
-}
-
-type JSONRPCResponse struct {
-	JSONRPC string `json:"jsonrpc"`
-	Result  struct {
-		Genesis json.RawMessage `json:"genesis"`
-	} `json:"result"`
 }
 
 func RemoveFileIfExists(filePath string) error {
