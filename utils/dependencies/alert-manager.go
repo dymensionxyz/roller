29155a4ebd25ce23713882e241719e22bc6e2023
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2025-02-20 22:17:30 +0200

    fix: typo

--- utils/dependencies/alert-manager.go
+++ utils/dependencies/alert-manager.go
@@ -12,7 +12,7 @@ func DefaultAlertAgentDependency() types.Dependency {
 		DependencyName:  "alert-agent",
 		RepositoryOwner: "dymensionxyz",
 		RepositoryName:  "alert-agent",
-		RepositoryUrl:   "https://github.com/dymensionxyz/alert-agent.git",
+		RepositoryUrl:   "https://github.com/dymensionxyz/alert-agent",
 		Release:         "v0.1.0-alpha-rc03",
 		Binaries: []types.BinaryPathPair{
 			{
