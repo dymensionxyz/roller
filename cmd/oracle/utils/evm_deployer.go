a80e62cb144bfabdb6a05ce0b3f8a4ade8fa1442
artpav <19916123+artemijspavlovs@users.noreply.github.com>
2025-03-03 09:22:18 +0200

    fix: price oracle usdt feed (#1303)

--- cmd/oracle/utils/evm_deployer.go
+++ cmd/oracle/utils/evm_deployer.go
@@ -242,7 +242,7 @@ func (e *EVMDeployer) DeployContract(
 					LocalNetworkName: common.HexToAddress(
 						"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
 					),
-					OracleNetworkName:     "USDC",
+					OracleNetworkName:     "USDT",
 					LocalNetworkPrecision: big.NewInt(6),
 				},
 			},
