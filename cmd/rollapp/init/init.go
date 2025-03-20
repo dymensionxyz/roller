77ec4c6400e66bb98f125a97d9ee9888bf11608d
anhductn2001 <ntd1312000@gmail.com>
2025-03-20 10:42:15 +0700

    fix: mock

--- cmd/rollapp/init/init.go
+++ cmd/rollapp/init/init.go
@@ -31,14 +31,20 @@ func Cmd() *cobra.Command {
 		Long:  ``,
 		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			pterm.Warning.Println(
-				"By default roller uses a public endpoint which is not reliable. for production usage it's highly recommended to use a private endpoint. A freemium private endpoint can be obtained in the following link https://blastapi.io/chains/dymension",
-			)
-			pterm.Info.Printf(
-				"run %s to update the Hub private endpoints anytime after initial setup\n",
-				pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
-					Sprintf("roller config set hub-rpc-endpoint <private-endpoint>"),
-			)
+			shouldUseMockBackend, _ := cmd.Flags().GetBool("mock")
+			shouldSkipBinaryInstallation, _ := cmd.Flags().GetBool("skip-binary-installation")
+
+			if !shouldUseMockBackend {
+				pterm.Warning.Println(
+					"By default roller uses a public endpoint which is not reliable. for production usage it's highly recommended to use a private endpoint. A freemium private endpoint can be obtained in the following link https://blastapi.io/chains/dymension",
+				)
+				pterm.Info.Printf(
+					"run %s to update the Hub private endpoints anytime after initial setup\n",
+					pterm.DefaultBasicText.WithStyle(pterm.FgYellow.ToStyle()).
+						Sprintf("roller config set hub-rpc-endpoint <private-endpoint>"),
+				)
+			}
+
 			err := initconfig.AddFlags(cmd)
 			if err != nil {
 				pterm.Error.Println("failed to initialize rollapp: ", err)
@@ -52,9 +58,6 @@ func Cmd() *cobra.Command {
 				pterm.Error.Println("failed to initialize rollapp: ", err)
 				return
 			}
-
-			shouldUseMockBackend, _ := cmd.Flags().GetBool("mock")
-			shouldSkipBinaryInstallation, _ := cmd.Flags().GetBool("skip-binary-installation")
 
 			// preflight checks
 			var hd consts.HubData
