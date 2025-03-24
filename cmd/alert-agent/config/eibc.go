4f31e2c04842292a4bdca57bf3949c06c03c2b9a
anhductn2001 <ntd1312000@gmail.com>
2025-03-24 11:45:43 +0700

    flag --rollapp a,b,c

--- cmd/alert-agent/config/eibc.go
+++ cmd/alert-agent/config/eibc.go
@@ -17,6 +17,8 @@ import (
 )
 
 func AddEibcLPCmd() *cobra.Command {
+	var rollappList string
+
 	cmd := &cobra.Command{
 		Use:  "add-eibc-lp <grantee-address> <threshold-amount>",
 		Args: cobra.ExactArgs(2),
@@ -48,41 +50,84 @@ func AddEibcLPCmd() *cobra.Command {
 			var config initam.AlertConfig
 			err = yaml.Unmarshal(configByte, config)
 			if err != nil {
-				return fmt.Errorf("failed to marshal config: %w", err)
+				return fmt.Errorf("failed to unmarshal config: %w", err)
 			}
 
 			grantsByGrantee, err := eibcutils.GetGrantsByGrantee(granteeAddr, rollerData.HubData)
 			if err != nil {
 				return fmt.Errorf("failed to get grants-by-gratee: %w", err)
 			}
-			for _, grant := range grantsByGrantee.Grants {
-				for _, rollapp := range grant.Authorization.Value.Rollapps {
-					for _, spendLimit := range rollapp.SpendLimit {
-						for _, addr := range config.Addresses {
-							if addr.Address == grant.Granter && addr.Threshold.Denom == spendLimit.Denom {
-								continue
-							}
+
+			var selectedRollapps []string
+			if rollappList != "" {
+				selectedRollapps = strings.Split(rollappList, ",")
+				for _, grant := range grantsByGrantee.Grants {
+					for _, rollapp := range grant.Authorization.Value.Rollapps {
+						if !contains(selectedRollapps, rollapp.RollappID) {
+							continue
 						}
-						config.Addresses = append(config.Addresses, initam.AddressConfig{
-							Name: fmt.Sprintf(
-								"%s LP wallet",
-								strings.ToUpper(rollerData.RollappID),
-							),
-							RestEndpoint: rollerData.HubData.ApiUrl,
-							Address:      grant.Granter,
-							Threshold: initam.AddressThreshold{
-								Denom:  spendLimit.Denom,
-								Amount: amount,
-							},
-						})
+						for _, spendLimit := range rollapp.SpendLimit {
+							for _, addr := range config.Addresses {
+								if addr.Address == grant.Granter && addr.Threshold.Denom == spendLimit.Denom {
+									continue
+								}
+							}
+							config.Addresses = append(config.Addresses, initam.AddressConfig{
+								Name: fmt.Sprintf(
+									"%s LP wallet",
+									strings.ToUpper(rollerData.RollappID),
+								),
+								RestEndpoint: rollerData.HubData.ApiUrl,
+								Address:      grant.Granter,
+								Threshold: initam.AddressThreshold{
+									Denom:  spendLimit.Denom,
+									Amount: amount,
+								},
+							})
+						}
 					}
 				}
+			} else {
+				for _, grant := range grantsByGrantee.Grants {
+					for _, rollapp := range grant.Authorization.Value.Rollapps {
+						for _, spendLimit := range rollapp.SpendLimit {
+							for _, addr := range config.Addresses {
+								if addr.Address == grant.Granter && addr.Threshold.Denom == spendLimit.Denom {
+									continue
+								}
+							}
+							config.Addresses = append(config.Addresses, initam.AddressConfig{
+								Name: fmt.Sprintf(
+									"%s LP wallet",
+									strings.ToUpper(rollerData.RollappID),
+								),
+								RestEndpoint: rollerData.HubData.ApiUrl,
+								Address:      grant.Granter,
+								Threshold: initam.AddressThreshold{
+									Denom:  spendLimit.Denom,
+									Amount: amount,
+								},
+							})
+						}
+					}
+				}
+			}
+
+			updatedConfig, err := yaml.Marshal(&config)
+			if err != nil {
+				return fmt.Errorf("failed to marshal updated config: %w", err)
+			}
+
+			err = os.WriteFile(configPath, updatedConfig, 0644)
+			if err != nil {
+				return fmt.Errorf("failed to write config file: %w", err)
 			}
 
 			return nil
 		},
 	}
 
+	cmd.Flags().StringVar(&rollappList, "rollapp", "", "Comma-separated list of rollapps exp: --rollapps a,b,c (default: all)")
 	return cmd
 }
 
@@ -132,9 +177,28 @@ func RemoveEibcLPCmd() *cobra.Command {
 				}
 			}
 
+			updatedConfig, err := yaml.Marshal(&config)
+			if err != nil {
+				return fmt.Errorf("failed to marshal updated config: %w", err)
+			}
+
+			err = os.WriteFile(configPath, updatedConfig, 0644)
+			if err != nil {
+				return fmt.Errorf("failed to write config file: %w", err)
+			}
+
 			return nil
 		},
 	}
 
 	return cmd
+}
+
+func contains(slice []string, item string) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
 }
