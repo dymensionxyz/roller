d4344b0bc8a41630389ad2e44ccde96f3db3b74c
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2025-03-27 13:12:57 +0200

    fix: transform the local network name into valid eth address

--- cmd/oracle/utils/evm_deployer.go
+++ cmd/oracle/utils/evm_deployer.go
@@ -232,30 +232,25 @@ func (e *EVMDeployer) DeployContract(
 			big.NewInt(3),
 			[]AssetInfo{
 				{
-					LocalNetworkName: common.HexToAddress(
-						"oracle/WBTC",
-					),
+					LocalNetworkName:      stringToAddress("oracle/WBTC"),
 					OracleNetworkName:     "WBTC",
 					LocalNetworkPrecision: big.NewInt(8),
 				},
+
 				{
-					LocalNetworkName: common.HexToAddress(
-						"oracle/USDC",
-					),
+					LocalNetworkName:      stringToAddress("oracle/USDC"),
 					OracleNetworkName:     "USDC",
 					LocalNetworkPrecision: big.NewInt(6),
 				},
+
 				{
-					LocalNetworkName: common.HexToAddress(
-						"oracle/USDT",
-					),
+					LocalNetworkName:      stringToAddress("oracle/USDT"),
 					OracleNetworkName:     "USDT",
 					LocalNetworkPrecision: big.NewInt(6),
 				},
+
 				{
-					LocalNetworkName: common.HexToAddress(
-						"oracle/DYM",
-					),
+					LocalNetworkName:      stringToAddress("oracle/DYM"),
 					OracleNetworkName:     "DYM",
 					LocalNetworkPrecision: big.NewInt(18),
 				},
@@ -689,4 +684,9 @@ func waitForEthTx(ethClient8545 *ethclient.Client, txHash common.Hash) *ethtypes
 	}
 
 	return nil
+}
+
+func stringToAddress(str string) common.Address {
+	hash := crypto.Keccak256Hash([]byte(str))
+	return common.BytesToAddress(hash.Bytes())
 }
