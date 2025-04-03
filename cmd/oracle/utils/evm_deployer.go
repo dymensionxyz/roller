b03c17d7949bf760332205e7fa863e085c64aa62
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2025-04-03 17:26:27 +0300

    fix: update fields

--- cmd/oracle/utils/evm_deployer.go
+++ cmd/oracle/utils/evm_deployer.go
@@ -255,7 +255,7 @@ func (e *EVMDeployer) DeployContract(
 					Precision:         big.NewInt(18),
 				},
 			},
-			5, // percentage, 0-100
+			big.NewInt(5), // percentage, 0-100
 			contractABI,
 		)
 		if err != nil {
@@ -353,7 +353,7 @@ func deployPriceOracleContract(
 	ecdsaPrivateKey *ecdsa.PrivateKey,
 	expirationOffset *big.Int,
 	assetInfos []AssetInfo,
-	boundThreshold int,
+	boundThreshold *big.Int,
 	contractABI string,
 ) (*goethcommon.Address, error) {
 	pterm.Info.Println("deploying Oracle contract")
