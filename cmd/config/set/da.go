25c0886d9c6c487389099ddd0522799b2e4025ba
ducnt131 <62016666+anhductn2001@users.noreply.github.com>
2025-04-15 16:37:07 +0700

    feat: config set da node (#1288)

--- cmd/config/set/da.go
+++ cmd/config/set/da.go
@@ -26,6 +26,87 @@ func setDA(rlpCfg roller.RollappConfig, value string) error {
 	return updateDaConfig(rlpCfg, daValue)
 }
 
+func setDARPC(rlpCfg roller.RollappConfig, value string) error {
+	daCfgDirPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.DALightNode)
+	_, err := filesystem.DirNotEmpty(daCfgDirPath)
+	if err != nil {
+		return err
+	}
+
+	rlpCfg.DA.RpcUrl = value
+	switch rlpCfg.DA.Backend {
+	case consts.Celestia:
+		return nil
+	case consts.Avail:
+		if err := sequencer.UpdateDymintDANodeConfig(rlpCfg, "endpoint"); err != nil {
+			return err
+		}
+	case consts.Local:
+		return fmt.Errorf("mock DA don't use RPC")
+
+	default:
+		return fmt.Errorf("unsupported DA backend: %s", rlpCfg.DA.Backend)
+	}
+
+	if err := roller.WriteConfig(rlpCfg); err != nil {
+		return err
+	}
+
+	fmt.Printf("💈 RollApp DA RPC has been successfully set to '%s'\n\n", value)
+	return nil
+}
+
+func setDAAPI(rlpCfg roller.RollappConfig, value string) error {
+	daCfgDirPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.DALightNode)
+	_, err := filesystem.DirNotEmpty(daCfgDirPath)
+	if err != nil {
+		return err
+	}
+
+	rlpCfg.DA.ApiUrl = value
+
+	switch rlpCfg.DA.Backend {
+	case consts.Celestia:
+		if err := roller.WriteConfig(rlpCfg); err != nil {
+			return err
+		}
+	case consts.Avail:
+		return fmt.Errorf("avail DA don't use API")
+	case consts.Local:
+		return fmt.Errorf("mock DA don't use API")
+	default:
+		return fmt.Errorf("unsupported DA backend: %s", rlpCfg.DA.Backend)
+	}
+
+	fmt.Printf("💈 RollApp DA API_URL has been successfully set to '%s'\n\n", value)
+	return nil
+}
+
+func setDAStateNode(rlpCfg roller.RollappConfig, value string) error {
+	daCfgDirPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.DALightNode)
+	_, err := filesystem.DirNotEmpty(daCfgDirPath)
+	if err != nil {
+		return err
+	}
+
+	rlpCfg.DA.CurrentStateNode = value
+
+	switch rlpCfg.DA.Backend {
+	case consts.Celestia:
+		if err := roller.WriteConfig(rlpCfg); err != nil {
+			return err
+		}
+	case consts.Avail:
+		return fmt.Errorf("avail DA don't use StateNode")
+	case consts.Local:
+		return fmt.Errorf("mock DA don't use StateNode")
+	default:
+		return fmt.Errorf("unsupported DA backend: %s", rlpCfg.DA.Backend)
+	}
+
+	fmt.Printf("💈 RollApp DA StateNode has been successfully set to '%s'\n\n", value)
+	return nil
+}
 func updateDaConfig(rlpCfg roller.RollappConfig, newDa consts.DAType) error {
 	daCfgDirPath := filepath.Join(rlpCfg.Home, consts.ConfigDirName.DALightNode)
 	dirExist, err := filesystem.DirNotEmpty(daCfgDirPath)
